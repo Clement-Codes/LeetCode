877bba4fc3d8c23101331edc2945c8acc9c3ae0b
Clement-Codes <clementcodes@gmail.com>
2024-09-13 21:31:16 +0100

    Time: 18 ms (7.23%) | Memory: 45.3 MB (6.37%) - LeetSync

---
+++ 36-valid-sudoku/valid-sudoku.java
@@ -0,0 +1,46 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Vector;
+
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        Vector<HashSet<Character>> validRow = new Vector<HashSet<Character>>();
+        Vector<HashSet<Character>> validCol = new Vector<HashSet<Character>>();
+        HashMap<Integer, HashSet<Character>> validBox  = new HashMap<Integer, HashSet<Character>>();
+
+        for(int i = 0; i < board.length; i++){
+            HashSet<Character> temp = new HashSet<Character>();
+            validRow.add(temp);
+            HashSet<Character> temp2 = new HashSet<Character>();
+            validCol.add(temp2);
+            for(int j = 0; j < board.length; j++){
+                int key = (i/3)*10 + (j/3);
+                HashSet<Character> temp3 = new HashSet<Character>();
+                validBox.put(key, temp3);
+            }
+            
+        }
+
+        for(int i = 0; i < board.length; i++){
+            for(int j = 0; j < board.length; j++){
+                int key = (i/3)*10 + (j/3);
+                if(board[i][j] == '.') continue;
+                if( validRow.get(i).contains(board[i][j]) ||  validCol.get(j).contains(board[i][j]) || validBox.get(key).contains(board[i][j])){
+                    System.out.println(validRow.get(i).contains(board[i][j]));
+                    System.out.println(validCol.get(j).contains(board[i][j]));
+                    System.out.println(validBox.get(key).contains(board[i][j]));
+                    System.out.println(validBox.get(0));
+                    System.out.println(validBox.get(1));
+                    System.out.println(board[i][j]);
+                    System.out.println(key);
+                    return false;
+                } 
+                validRow.get(i).add(board[i][j]);
+                validCol.get(j).add(board[i][j]);
+                validBox.get(key).add(board[i][j]);
+                
+            }
+        }
+        return true;
+    }
+}
