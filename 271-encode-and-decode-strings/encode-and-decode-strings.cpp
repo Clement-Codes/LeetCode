00ac092b82cf1d4b055c905fef018850ece0e9fc
Clement-Codes <clementcodes@gmail.com>
2024-09-13 10:40:43 +0100

    Time: 28 ms (79.18%) | Memory: 27.7 MB (44.03%) - LeetSync

---
+++ 271-encode-and-decode-strings/encode-and-decode-strings.cpp
@@ -0,0 +1,35 @@
+class Codec {
+public:
+
+    // Encodes a list of strings to a single string.
+    string encode(vector<string>& strs) {
+        string encoded = "";
+        for(string s: strs){
+            encoded += to_string(s.length()) + '#' + s; 
+        }
+        cout << encoded;
+        return encoded;
+    }
+
+    // Decodes a single string to a list of strings.
+    vector<string> decode(string s) {
+        vector<string> decoded;
+        int i = 0;
+        while(i<s.length()){
+            int j = i;
+            while(s[j]!='#'){
+                j+=1;
+            }
+            string in = s.substr(i, j-i);
+            int index = stoi(in);
+            cout<<in;
+            decoded.push_back(s.substr(j+1, index));
+            i = j+1+index;
+        }
+        return decoded;
+    }
+};
+
+// Your Codec object will be instantiated and called as such:
+// Codec codec;
+// codec.decode(codec.encode(strs));
