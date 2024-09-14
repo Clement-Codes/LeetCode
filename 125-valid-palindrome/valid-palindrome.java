9efb3198e6a51fae31d5216c0ab3460d2e24f89b
Clement-Codes <clementcodes@gmail.com>
2024-09-14 12:07:46 +0100

    Time: 2 ms (99.14%) | Memory: 42.7 MB (91.47%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,9 +1,13 @@
 class Solution {
+    public boolean isLetterOrDigit(char c){
+        if (((int)'A' <= (int)c && (int)c <= (int)'Z') || ((int)'a' <= (int)c && (int)c <= (int)'z') || ((int)'0' <= (int)c && (int)c <= (int)'9')) return true;
+        return false;
+    }
     public boolean isPalindrome(String s) {
         int i = 0;
         int j = s.length() - 1;
         while (i < j){
-            while ((i < j) && !Character.isLetterOrDigit(s.charAt(i))) i += 1;
+            while ((i < j) && !isLetterOrDigit(s.charAt(i))) i += 1;
             while ((i < j) && !Character.isLetterOrDigit(s.charAt(j))) j -= 1;
             if ( Character.toLowerCase(s.charAt(i)) != Character.toLowerCase(s.charAt(j)))return false;
             
