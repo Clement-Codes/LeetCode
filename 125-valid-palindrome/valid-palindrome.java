297444268cf58e63995eeb0bb37a125ddf1c5689
Clement-Codes <clementcodes@gmail.com>
2024-09-14 12:03:27 +0100

    Time: 2 ms (99.14%) | Memory: 42.6 MB (91.47%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -5,9 +5,8 @@ class Solution {
         while (i < j){
             while ((i < j) && !Character.isLetterOrDigit(s.charAt(i))) i += 1;
             while ((i < j) && !Character.isLetterOrDigit(s.charAt(j))) j -= 1;
-            if ( Character.toLowerCase(s.charAt(i)) != Character.toLowerCase(s.charAt(j))){
-                return false;
-            }
+            if ( Character.toLowerCase(s.charAt(i)) != Character.toLowerCase(s.charAt(j)))return false;
+            
             i += 1;
             j -= 1;
         }
