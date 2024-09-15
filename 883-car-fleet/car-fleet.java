4ff74f8f962cd38cb50ebedff621550e9d694d43
Clement-Codes <clementcodes@gmail.com>
2024-09-15 21:24:23 +0100

    Time: 640 ms (5.00%) | Memory: 56.2 MB (88.56%) - LeetSync

---
+++ 883-car-fleet/car-fleet.java
@@ -0,0 +1,29 @@
+import java.util.Collection;
+import java.util.Stack;
+
+class Solution {
+    public int carFleet(int target, int[] position, int[] speed) {
+        Stack<Double> s = new Stack<Double>();
+        int cars [][] = new int[position.length][2];
+
+        for(int i = 0; i < position.length; i++){
+            cars[i][0] = position[i];
+            cars[i][1] = speed[i];
+        }
+
+        Arrays.sort(cars, (a,b) -> Integer.compare(b[0], a[0]));
+
+        for(int car[]: cars){
+            double p = (target - car[0])/(double)car[1];
+            System.out.println(p);
+            if (s.size()>0 && p<=s.peek()){
+                continue;
+            }
+            else{
+                s.push(p);
+            }
+        }
+
+        return s.size();
+    }
+}
