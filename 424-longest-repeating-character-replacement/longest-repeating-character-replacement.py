38f1cd2787d9f143042ceefc8684abb5bad643d5
Clement-Codes <clementcodes@gmail.com>
2024-09-16 09:39:06 +0100

    Time: 81 ms (76.85%) | Memory: 16.7 MB (38.09%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -10,7 +10,5 @@ class Solution:
             while (r - l + 1) - mostFrequent > k:
                 dictionary[s[l]] -=1
                 l += 1
-            print(l, r)
             result = max(result, r - l + 1)
-        print(dictionary, r, l)
         return result
