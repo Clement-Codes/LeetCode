3841856d9ee4808a4dae97ea23b0d1cda2e4c300
Clement-Codes <clementcodes@gmail.com>
2024-09-20 10:30:39 +0100

    Time: 34 ms (89.54%) | Memory: 17.5 MB (82.65%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.py
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.py
@@ -7,7 +7,23 @@
 
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
-        if not root: return 0
+        #recursive BFS
 
-        return (1 + max(self.maxDepth(root.left), self.maxDepth(root.right)))
+        # if not root: return 0
+        # return (1 + max(self.maxDepth(root.left), self.maxDepth(root.right)))
         
+        #iterative BFS
+        if not root: return 0
+        queue = deque([root])
+        result = 0
+
+        while queue:
+            for i in range(len(queue)):
+                node = queue.popleft()
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            result += 1
+        
+        return result
