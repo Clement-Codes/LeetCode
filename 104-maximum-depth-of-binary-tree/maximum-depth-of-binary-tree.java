404a652700a0ece7511972476db78f176167dee1
Clement-Codes <clementcodes@gmail.com>
2024-09-20 11:17:28 +0100

    Time: 4 ms (11.45%) | Memory: 43 MB (6.03%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.java
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.java
@@ -25,49 +25,49 @@ class Solution {
     // }
 
     // BFS iterative
-    public int maxDepth(TreeNode root) {
-        if (root == null) return 0;
-        Queue<TreeNode> q = new LinkedList<>();
-        q.add(root);
-        int result = 0;
-        while(q.size() > 0){
-            int len = q.size();
-            for(int i = 0; i<len;i++){
-                TreeNode temp = q.peek();
-                q.remove();
-                if (temp.left != null){
-                    q.add(temp.left);
-                }
-                if (temp.right  != null){
-                    q.add(temp.right);
-                }
-            }
-            result += 1;
-        }
-        return result;
-    }
-
-    // DFS iterative
-    // int maxDepth(TreeNode* root) {
-    //     stack<pair<TreeNode*, int>> s;
-    //     pair<TreeNode*, int> head (root, 1);
-    //     s.push(head);
+    // public int maxDepth(TreeNode root) {
+    //     if (root == null) return 0;
+    //     Queue<TreeNode> q = new LinkedList<>();
+    //     q.add(root);
     //     int result = 0;
-    //     while (s.size()){
-    //         pair<TreeNode*, int> temp = s.top();
-    //         s.pop();
-    //         if(temp.first){
-    //             if(temp.first->left){
-    //                 pair<TreeNode*, int> t (temp.first->left, 1 + temp.second);
-    //                 s.push(t);
+    //     while(q.size() > 0){
+    //         int len = q.size();
+    //         for(int i = 0; i<len;i++){
+    //             TreeNode temp = q.peek();
+    //             q.remove();
+    //             if (temp.left != null){
+    //                 q.add(temp.left);
     //             }
-    //             if(temp.first->right){
-    //                 pair<TreeNode*, int> t (temp.first->right, 1 + temp.second);
-    //                 s.push(t);
-    //             }          
-    //             result = max(result, temp.second);
+    //             if (temp.right  != null){
+    //                 q.add(temp.right);
+    //             }
     //         }
+    //         result += 1;
     //     }
     //     return result;
     // }
+
+    // DFS iterative
+    public int maxDepth(TreeNode root) {
+        Stack<Pair<TreeNode, Integer>> s = new Stack<>();
+        Pair<TreeNode, Integer> head  = new Pair<TreeNode, Integer>(root, 1);
+        s.push(head);
+        int result = 0;
+        while (s.size() > 0 ){
+            Pair<TreeNode, Integer> temp = s.peek();
+            s.pop();
+            if(temp.getKey() != null){
+                if(temp.getKey().left != null){
+                    Pair<TreeNode, Integer> t = new Pair<TreeNode, Integer>(temp.getKey().left, 1 + temp.getValue());
+                    s.push(t);
+                }
+                if(temp.getKey().right != null){
+                    Pair<TreeNode, Integer> t = new Pair<TreeNode, Integer>(temp.getKey().right, 1 + temp.getValue());
+                    s.push(t);
+                }          
+                result = Math.max(result, temp.getValue());
+            }
+        }
+        return result;
+    }
 }
