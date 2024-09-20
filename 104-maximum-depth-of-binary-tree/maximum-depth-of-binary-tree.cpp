0b731d7ea4adcc05b455a874a1fca38eccd4b2e0
Clement-Codes <clementcodes@gmail.com>
2024-09-20 10:54:33 +0100

    Time: 8 ms (36.32%) | Memory: 17.7 MB (46.37%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
@@ -9,10 +9,40 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+#include<queue>
+#include<stack>
 class Solution {
 public:
+    // BFS recursive
+    // int maxDepth(TreeNode* root) {
+    //     if (!root) return 0;
+    //     return (1 + max(maxDepth(root->left), maxDepth(root->right)));
+    // }
+
+    // BFS iterative
     int maxDepth(TreeNode* root) {
         if (!root) return 0;
-        return (1 + max(maxDepth(root->left), maxDepth(root->right)));
+        queue<TreeNode*> q;
+        q.push(root);
+        int result = 0;
+        while(q.size()){
+            cout << q.size() <<": ";
+            int len = q.size();
+            for(int i = 0; i<len;i++){
+                TreeNode* temp = q.front();
+                cout << temp->val << ", " ;
+                q.pop();
+                if (temp->left){
+                    q.push(temp->left);
+                }
+                if (temp->right){
+                    q.push(temp->right);
+                }
+            }
+            cout<<"\n";
+            result += 1;
+        }
+        return result;
     }
 };
