9795fa2ff666932502d218e34076441594e72942
Clement-Codes <clementcodes@gmail.com>
2024-09-20 11:07:36 +0100

    Time: 0 ms (100.00%) | Memory: 17.9 MB (8.24%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
@@ -21,27 +21,51 @@ public:
     // }
 
     // BFS iterative
+    // int maxDepth(TreeNode* root) {
+    //     if (!root) return 0;
+    //     queue<TreeNode*> q;
+    //     q.push(root);
+    //     int result = 0;
+    //     while(q.size()){
+    //         cout << q.size() <<": ";
+    //         int len = q.size();
+    //         for(int i = 0; i<len;i++){
+    //             TreeNode* temp = q.front();
+    //             cout << temp->val << ", " ;
+    //             q.pop();
+    //             if (temp->left){
+    //                 q.push(temp->left);
+    //             }
+    //             if (temp->right){
+    //                 q.push(temp->right);
+    //             }
+    //         }
+    //         cout<<"\n";
+    //         result += 1;
+    //     }
+    //     return result;
+    // }
+
+    // DFS iterative
     int maxDepth(TreeNode* root) {
-        if (!root) return 0;
-        queue<TreeNode*> q;
-        q.push(root);
+        stack<pair<TreeNode*, int>> s;
+        pair<TreeNode*, int> head (root, 1);
+        s.push(head);
         int result = 0;
-        while(q.size()){
-            cout << q.size() <<": ";
-            int len = q.size();
-            for(int i = 0; i<len;i++){
-                TreeNode* temp = q.front();
-                cout << temp->val << ", " ;
-                q.pop();
-                if (temp->left){
-                    q.push(temp->left);
+        while (s.size()){
+            pair<TreeNode*, int> temp = s.top();
+            s.pop();
+            if(temp.first){
+                if(temp.first->left){
+                    pair<TreeNode*, int> t (temp.first->left, 1 + temp.second);
+                    s.push(t);
                 }
-                if (temp->right){
-                    q.push(temp->right);
-                }
+                if(temp.first->right){
+                    pair<TreeNode*, int> t (temp.first->right, 1 + temp.second);
+                    s.push(t);
+                }          
+                result = max(result, temp.second);
             }
-            cout<<"\n";
-            result += 1;
         }
         return result;
     }
