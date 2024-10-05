a6801365160591ec553138c9ef63adda7967b0db
Clement-Codes <clementcodes@gmail.com>
2024-10-05 23:46:49 +0100

    Time: 0 ms (100.00%) | Memory: 7.4 MB (96.64%) - LeetSync

---
+++ 62-unique-paths/unique-paths.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        int* row = new int [n];
+        fill_n(row, n, 1);
+        for (int i = 0; i < m - 1; i++)
+            for (int j = n - 2; j >= 0; j--)
+                row[j] = row[j] + row[j+1];
+        return row[0];
+    }
+};
