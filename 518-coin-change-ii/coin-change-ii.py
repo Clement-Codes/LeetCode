5ca5aacb94d634ad17916b5bb4f6688fbfed3f4d
Clement-Codes <clementcodes@gmail.com>
2024-10-06 15:37:00 +0100

    Time: 474 ms (28.33%) | Memory: 48 MB (30.99%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.py
+++ 518-coin-change-ii/coin-change-ii.py
@@ -1,25 +1,25 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
-        # df = [[0 for _ in range(len(coins) + 1)] for _ in range(amount + 1)]
-        # df[0] = [1 for _ in range(len(coins) + 1)]
+        df = [[0 for _ in range(len(coins) + 1)] for _ in range(amount + 1)]
+        df[0] = [1 for _ in range(len(coins) + 1)]
 
-        # for i in range(1, amount + 1):
-        #     for j in range(len(coins) - 1, -1, -1):
-        #         df[i][j] = df[i][j+1]
-        #         if i - coins[j]>=0:
-        #             df[i][j] += df[i - coins[j]][j]
+        for i in range(1, amount + 1):
+            for j in range(len(coins) - 1, -1, -1):
+                df[i][j] = df[i][j+1]
+                if i - coins[j]>=0:
+                    df[i][j] += df[i - coins[j]][j]
 
 
-        # return df[amount][0]
+        return df[amount][0]
 
-        catche = {}
+        # catche = {}
 
-        def dfs(i, a):
-            if a == amount: return 1
-            if a > amount: return 0
-            if i == len(coins): return 0
-            if (i, a) in catche: return catche[(i, a)]
+        # def dfs(i, a):
+        #     if a == amount: return 1
+        #     if a > amount: return 0
+        #     if i == len(coins): return 0
+        #     if (i, a) in catche: return catche[(i, a)]
 
-            catche[(i, a)] = dfs(i+1, a) + dfs(i, a + coins[i])
-            return catche[(i, a)]
-        return dfs(0,0)
+        #     catche[(i, a)] = dfs(i+1, a) + dfs(i, a + coins[i])
+        #     return catche[(i, a)]
+        # return dfs(0,0)
