850e3f558125621740035a828dd8405d6da7e255
Clement-Codes <clementcodes@gmail.com>
2024-10-06 17:17:48 +0100

    Time: 15 ms (14.56%) | Memory: 51 MB (5.26%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.java
+++ 518-coin-change-ii/coin-change-ii.java
@@ -1,17 +1,33 @@
 class Solution {
-    HashMap<String, Integer> cache = new HashMap<String, Integer>();
+    // HashMap<String, Integer> cache = new HashMap<String, Integer>();
 
-    public int dfs(int i, int a, int amount, int[] coins){
-        if (a == amount) return 1;
-        if (a > amount) return 0;
-        if (i == coins.length) return 0;
-        String key = String.valueOf(i) + "_" + String.valueOf(a);
-        if (cache.containsKey(key)) return cache.get(key);
-        cache.put(key, dfs(i+1, a, amount, coins) + dfs(i, a + coins[i], amount, coins));
-        return cache.get(key);
-    }
+    // public int dfs(int i, int a, int amount, int[] coins){
+    //     if (a == amount) return 1;
+    //     if (a > amount) return 0;
+    //     if (i == coins.length) return 0;
+    //     String key = String.valueOf(i) + "_" + String.valueOf(a);
+    //     if (cache.containsKey(key)) return cache.get(key);
+    //     cache.put(key, dfs(i+1, a, amount, coins) + dfs(i, a + coins[i], amount, coins));
+    //     return cache.get(key);
+    // }
+
+    // public int change(int amount, int[] coins) {
+    //     return dfs(0 , 0, amount, coins);    
+    // }
 
     public int change(int amount, int[] coins) {
-        return dfs(0 , 0, amount, coins);    
+        int dp[][] = new int[amount + 1][coins.length + 1];
+        for (int i = 0; i < amount + 1; i++){
+            if (i == 0) Arrays.fill(dp[i], 1);
+            else Arrays.fill(dp[i], 0);
+        }
+
+        for (int i = 1; i < amount + 1; i++){
+            for (int j = coins.length - 1; j >= 0; j--){
+                dp[i][j] = dp[i][j+1];
+                if (i - coins[j] >= 0) dp[i][j] += dp[i - coins[j]][j];
+            }
+        }
+        return dp[amount][0];
     }
 }
