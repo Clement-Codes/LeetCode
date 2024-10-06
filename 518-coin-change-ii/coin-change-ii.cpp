c5074ea9b873c2b660ac76efe1cc6af29c996b5c
Clement-Codes <clementcodes@gmail.com>
2024-10-06 17:40:35 +0100

    Time: 11 ms (59.42%) | Memory: 44.5 MB (6.48%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -17,27 +17,47 @@ public:
     //     // return dfs(0,0, amount, coins);
     // }
 
-    int change(int amount, vector<int>& coins) {
-        unsigned long long** dp = new unsigned long long*[amount+1];
+    // int change(int amount, vector<int>& coins) {
+    //     unsigned long long** dp = new unsigned long long*[amount+1];
     
-        for (int i = 0; i < amount + 1; i++){
-            dp[i] = new unsigned long long[coins.size() + 1];
-            if (i == 0){
-                fill_n(dp[i], coins.size() + 1, 1);
-            }
-            else{
-                fill_n(dp[i], coins.size() + 1, 0);
-            }
-        }
+    //     for (int i = 0; i < amount + 1; i++){
+    //         dp[i] = new unsigned long long[coins.size() + 1];
+    //         if (i == 0){
+    //             fill_n(dp[i], coins.size() + 1, 1);
+    //         }
+    //         else{
+    //             fill_n(dp[i], coins.size() + 1, 0);
+    //         }
+    //     }
 
-        for (int i = 1; i < amount + 1; i++){
-            for (int j = coins.size() - 1; j >=0 ; j--){
-                dp[i][j] = dp[i][j+1];
-                if (i - coins[j] >=0){
-                    dp[i][j] += dp[i - coins[j]][j];
+    //     for (int i = 1; i < amount + 1; i++){
+    //         for (int j = coins.size() - 1; j >=0 ; j--){
+    //             dp[i][j] = dp[i][j+1];
+    //             if (i - coins[j] >=0){
+    //                 dp[i][j] += dp[i - coins[j]][j];
+    //             }
+    //         }
+    //     }
+    //     return dp[amount][0];
+    // }
+
+    int change(int amount, vector<int>& coins) {
+        unsigned long long* dp = new unsigned long long[amount + 1];
+        fill_n(dp, amount+1, 0);
+        dp[0] = 1;
+        for (int i = coins.size() - 1; i >= 0; i--){
+            unsigned long long* temp = new unsigned long long[amount + 1];
+            fill_n(temp, amount+1, 0);
+            temp[0] = 1;
+            for (int a = 1; a < amount + 1; a++){
+                temp[a] = dp[a];
+                if (a - coins[i] >= 0)
+                {
+                    temp[a] += temp[a - coins[i]];
                 }
             }
+            dp = temp;
         }
-        return dp[amount][0];
+        return dp[amount];
     }
 };
