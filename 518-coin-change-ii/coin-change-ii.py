f7076e59bf821751e958dd624792f5007dab0f95
Clement-Codes <clementcodes@gmail.com>
2024-10-06 15:47:37 +0100

    Time: 341 ms (38.04%) | Memory: 17 MB (60.45%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.py
+++ 518-coin-change-ii/coin-change-ii.py
@@ -1,16 +1,28 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
-        df = [[0 for _ in range(len(coins) + 1)] for _ in range(amount + 1)]
-        df[0] = [1 for _ in range(len(coins) + 1)]
+        df = [0 for _ in range(amount + 1) ]
+        df[0] = 1
+        for i in range(len(coins) - 1, -1, -1):
+            temp = [0 for _ in range(amount + 1)]
+            temp[0] = 1
+            for j in range(1, amount + 1):
+                temp[j] = df[j]
+                if j - coins[i] >=0:
+                    temp[j] += temp[j - coins[i]]
+            df = temp
+        return df[amount] 
 
-        for i in range(1, amount + 1):
-            for j in range(len(coins) - 1, -1, -1):
-                df[i][j] = df[i][j+1]
-                if i - coins[j]>=0:
-                    df[i][j] += df[i - coins[j]][j]
+        # df = [[0 for _ in range(len(coins) + 1)] for _ in range(amount + 1)]
+        # df[0] = [1 for _ in range(len(coins) + 1)]
+
+        # for i in range(1, amount + 1):
+        #     for j in range(len(coins) - 1, -1, -1):
+        #         df[i][j] = df[i][j+1]
+        #         if i - coins[j]>=0:
+        #             df[i][j] += df[i - coins[j]][j]
 
 
-        return df[amount][0]
+        # return df[amount][0]
 
         # catche = {}
 
