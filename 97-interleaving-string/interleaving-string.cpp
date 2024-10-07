d691bd04d168ddc56875f006de13ac783b61a6f0
Clement-Codes <clementcodes@gmail.com>
2024-10-07 10:01:29 +0100

    Time: 5 ms (52.23%) | Memory: 8.1 MB (87.13%) - LeetSync

--- 97-interleaving-string/interleaving-string.cpp
+++ 97-interleaving-string/interleaving-string.cpp
@@ -1,19 +1,31 @@
 class Solution {
 public:
-    unordered_map<string, bool> cache;
+//    unordered_map<string, bool> cache;
 
-    bool dfs(int i, int j, string s1, string s2, string s3){
-        if (i == s1.length() && j == s2.length()) return true;
-        string key = to_string(i) + "_" + to_string(j);
-        if (cache.contains(key)) return cache[key];
-        if (i < s1.length() && s1[i] == s3[i+j] && dfs(i+1, j, s1, s2, s3)) return true;
-        if (j < s2.length() && s2[j] == s3[i+j] && dfs(i, j+1, s1, s2, s3)) return true;
-        cache[key] = false;
-        return cache[key];
-    } 
+    // bool dfs(int i, int j, string s1, string s2, string s3){
+    //     if (i == s1.length() && j == s2.length()) return true;
+    //     string key = to_string(i) + "_" + to_string(j);
+    //     if (cache.contains(key)) return cache[key];
+    //     if (i < s1.length() && s1[i] == s3[i+j] && dfs(i+1, j, s1, s2, s3)) return true;
+    //     if (j < s2.length() && s2[j] == s3[i+j] && dfs(i, j+1, s1, s2, s3)) return true;
+    //     cache[key] = false;
+    //     return cache[key];
+    // } 
 
     bool isInterleave(string s1, string s2, string s3) {
         if (s1.length() + s2.length() != s3.length()) return false;
-        return dfs(0, 0, s1, s2, s3);
+        // return dfs(0, 0, s1, s2, s3);
+        bool** dp = new bool*[s1.length() + 1];
+        for (int i = 0; i < s1.length() + 1; i++){
+            dp[i] = new bool[s2.length() + 1]();
+        }
+        dp[s1.length()][s2.length()] = true;
+        for (int i = s1.length(); i >= 0; i--){
+            for (int j = s2.length(); j >= 0; j--){
+                if (i < s1.length() && s1[i] == s3[i+j] && dp[i + 1][j]) dp[i][j] = true;
+                if (j < s2.length() && s2[j] == s3[i+j] && dp[i][j + 1]) dp[i][j] = true;
+            }
+        }
+        return dp[0][0];
     }
 };
