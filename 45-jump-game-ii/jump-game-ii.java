943e0f9550d2e75fe234c95b721d7597e502dabc
Clement-Codes <clementcodes@gmail.com>
2024-10-08 21:08:33 +0100

    Time: 1 ms (98.48%) | Memory: 44.9 MB (75.81%) - LeetSync

---
+++ 45-jump-game-ii/jump-game-ii.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int jump(int[] nums) {
+        int result = 0;
+        int l = 0, r = 0;
+        while (r < nums.length - 1){
+            int largest = 0;
+            for (int i = l; i < r + 1; i++){
+                largest = Math.max(largest, i + nums[i]);
+            }
+            l = r + 1;
+            r = largest;
+            result += 1;
+        }
+        return result;
+    }
+}
