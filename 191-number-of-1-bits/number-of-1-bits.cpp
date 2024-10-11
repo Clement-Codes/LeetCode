cd30e26969b13d35ef1a19febac78ac5f95a43b0
Clement-Codes <clementcodes@gmail.com>
2024-10-11 12:23:25 +0100

    Time: 4 ms (30.88%) | Memory: 7.9 MB (34.97%) - LeetSync

--- 191-number-of-1-bits/number-of-1-bits.cpp
+++ 191-number-of-1-bits/number-of-1-bits.cpp
@@ -1,10 +1,17 @@
 class Solution {
 public:
     int hammingWeight(int n) {
+        // int result = 0;
+        // while (n > 0){
+        //     result += n%2;
+        //     n = n >> 1;
+        // }
+        // return result;
+
         int result = 0;
         while (n > 0){
-            result += n%2;
-            n = n >> 1;
+            n &= n-1;
+            result += 1;
         }
         return result;
     }
