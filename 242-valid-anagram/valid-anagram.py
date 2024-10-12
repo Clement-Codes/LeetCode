7e20a8c1e7f21593a48fe047e41d7b5a30f1f4c2
Clement-Codes <clementcodes@gmail.com>
2024-10-12 10:30:01 +0100

    Time: 44 ms (79.54%) | Memory: 17 MB (56.88%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -2,15 +2,38 @@ class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         if len(s) != len(t):
             return False
-        h_s, h_t = {}, {}
-        for i in range(len(s)):
-            h_s[s[i]] = 1 + h_s.get(s[i],0)
-            h_t[t[i]] = 1 + h_t.get(t[i],0)
         
-        for i in h_s.keys():
-            if h_s[i] != h_t.get(i, 0):
-                return False
-        return True
+        count_s = {}
+        count_t = {}
+
+        for i in range(len(s)):
+            count_s[s[i]] = 1 + count_s.get(s[i], 0)
+            count_t[t[i]] = 1 + count_t.get(t[i], 0)
+
+        return count_s == count_t
+
+
+
+
+
+
+
+
+
+
+
+
+        # if len(s) != len(t):
+        #     return False
+        # h_s, h_t = {}, {}
+        # for i in range(len(s)):
+        #     h_s[s[i]] = 1 + h_s.get(s[i],0)
+        #     h_t[t[i]] = 1 + h_t.get(t[i],0)
+        
+        # for i in h_s.keys():
+        #     if h_s[i] != h_t.get(i, 0):
+        #         return False
+        # return True
         
         #print(h_s)
         # for i in s:
