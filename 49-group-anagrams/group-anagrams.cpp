6f752cfbc63b0413d0e0e48676e0facd569df56e
Clement-Codes <clementcodes@gmail.com>
2024-10-14 02:52:43 +0100

    Time: 39 ms (20.44%) | Memory: 35.1 MB (6.32%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -3,20 +3,33 @@ class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         vector<vector<string>> result;
-        map<vector<int>, vector<string>> results;
-        for(string s: strs){
-            vector<int> count;
-            for(int i = 0; i < 26; i++){
-                count.push_back(0);
+        map<vector<int>, vector<string>> cache;
+        for (string str: strs){
+            vector<int> temp (26, 0);
+            for (char i: str){
+                temp[i - 'a'] += 1;
             }
-            for(char c: s){
-                count[int(c) - int('a')] += 1;
-            }
-            results[count].push_back(s);
+            cache[temp].push_back(str);
         }
-        for(const auto i : results){
+        for (pair<vector<int>, vector<string>> i: cache){
             result.push_back(i.second);
         }
         return result;
+        // vector<vector<string>> result;
+        // map<vector<int>, vector<string>> results;
+        // for(string s: strs){
+        //     vector<int> count;
+        //     for(int i = 0; i < 26; i++){
+        //         count.push_back(0);
+        //     }
+        //     for(char c: s){
+        //         count[int(c) - int('a')] += 1;
+        //     }
+        //     results[count].push_back(s);
+        // }
+        // for(const auto i : results){
+        //     result.push_back(i.second);
+        // }
+        // return result;
     }
 };
