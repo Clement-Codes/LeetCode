e6fd23b148068392285b044e242bdca052f99886
Clement-Codes <clementcodes@gmail.com>
2024-10-15 09:48:20 +0100

    Time: 7 ms (100.00%) | Memory: 45.4 MB (54.12%) - LeetSync

--- 3195-separate-black-and-white-balls/separate-black-and-white-balls.java
+++ 3195-separate-black-and-white-balls/separate-black-and-white-balls.java
@@ -1,13 +1,23 @@
 class Solution {
     public long minimumSteps(String s) {
         long swaps = 0;
-        long white = 0;
-        for (int i = 0; i < s.length(); i++){
-            if (s.charAt(i) == '0'){
-                swaps += i - white;
-                white += 1;
+        long black = 0;
+        for (char c: s.toCharArray()){
+            if (c == '1'){
+                black += 1;
+            }
+            else{
+                swaps += black;
             }
         }
         return swaps;
+        // long white = 0;
+        // for (int i = 0; i < s.length(); i++){
+        //     if (s.charAt(i) == '0'){
+        //         swaps += i - white;
+        //         white += 1;
+        //     }
+        // }
+        // return swaps;
     }
 }
