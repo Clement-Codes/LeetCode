e7086ea72e1fb6b68481fa6d1b8e431660de2551
Clement-Codes <clementcodes@gmail.com>
2024-10-15 09:43:21 +0100

    Time: 35 ms (43.16%) | Memory: 15.7 MB (14.11%) - LeetSync

--- 3195-separate-black-and-white-balls/separate-black-and-white-balls.cpp
+++ 3195-separate-black-and-white-balls/separate-black-and-white-balls.cpp
@@ -2,14 +2,24 @@ class Solution {
 public:
     long long minimumSteps(string s) {
         long long swaps = 0;
-        long long white = 0;
-        for (int i = 0; i < s.length(); i++){
-            if (s[i] == '0'){
-                swaps += i - white;
-                white += 1;
+        //long long white = 0;
+        // for (int i = 0; i < s.length(); i++){
+        //     if (s[i] == '0'){
+        //         swaps += i - white;
+        //         white += 1;
+        //     }
+        // }
+
+        // return swaps;
+        long long black = 0;
+        for (char c: s){
+            if (c == '1'){
+                black += 1;
+            }
+            else{
+                swaps += black;
             }
         }
-
         return swaps;
     }
 };
