e18a94856d36b6937fe01e9b70aa32759299a68e
Clement-Codes <clementcodes@gmail.com>
2024-10-15 23:08:11 +0100

    Time: 86 ms (71.56%) | Memory: 22.1 MB (17.08%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,18 +1,61 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        count = defaultdict(int)
-        for i in nums:
-            count[i] += 1
+        counts = defaultdict(int)
+        for num in nums:
+            counts[num] += 1
         bucket = [[] for i in range(len(nums) + 1)]
-        for i,v in count.items():
-            bucket[v].append(i)
-        
+        for i, j in counts.items():
+            bucket[j].append(i)
         result = []
-
-        for i in range(len(bucket)-1, 0 , -1):
-            for j in bucket[i]:
-                result.append(j)
-            if len(result) == k:
-                break
-        
+        for i in range(len(nums), -1, -1):
+            if(bucket[i]): result.extend(bucket[i])
+            if len(result) == k: break
         return result
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # count = defaultdict(int)
+        # for i in nums:
+        #     count[i] += 1
+        # bucket = [[] for i in range(len(nums) + 1)]
+        # for i,v in count.items():
+        #     bucket[v].append(i)
+        
+        # result = []
+
+        # for i in range(len(bucket)-1, 0 , -1):
+        #     for j in bucket[i]:
+        #         result.append(j)
+        #     if len(result) == k:
+        #         break
+        
+        # return result
