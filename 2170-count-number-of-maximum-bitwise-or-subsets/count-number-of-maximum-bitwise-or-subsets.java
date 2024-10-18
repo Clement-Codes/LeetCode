412846f03ef3c23386333b784635588a01981a9b
Clement-Codes <clementcodes@gmail.com>
2024-10-18 20:54:31 +0100

    Time: 9 ms (67.33%) | Memory: 41.1 MB (55.40%) - LeetSync

--- 2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.java
+++ 2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.java
@@ -1,22 +1,37 @@
 class Solution {
-    int result = 0;
-    public void dfs(int index, int curresult, int[] nums, int max_or){
+    // Brute Force
+    // int result = 0;
+    // public void dfs(int index, int curresult, int[] nums, int max_or){
+    //     if(index == nums.length){
+    //         if(curresult == max_or){
+    //             result += 1;
+    //         }
+    //         return;
+    //     }
+    //     dfs(index + 1, curresult, nums, max_or);
+    //     dfs(index + 1, curresult | nums[index], nums, max_or);
+    // }
+
+    // public int countMaxOrSubsets(int[] nums) {
+    //     int max_or = 0;
+    //     for (int i: nums)
+    //         max_or |= i;
+        
+    //     dfs(0, 0, nums, max_or);
+    //     return result;
+    // }
+
+    public int dfs(int index, int curresult, int[] nums, int max_or){
         if(index == nums.length){
-            if(curresult == max_or){
-                result += 1;
-            }
-            return;
+            return curresult == max_or ? 1 : 0;
         }
-        dfs(index + 1, curresult, nums, max_or);
-        dfs(index + 1, curresult | nums[index], nums, max_or);
+        return dfs(index + 1, curresult, nums, max_or) + dfs(index + 1, curresult | nums[index], nums, max_or);
     }
 
     public int countMaxOrSubsets(int[] nums) {
         int max_or = 0;
         for (int i: nums)
             max_or |= i;
-        
-        dfs(0, 0, nums, max_or);
-        return result;
+        return dfs(0, 0, nums, max_or);
     }
 }
