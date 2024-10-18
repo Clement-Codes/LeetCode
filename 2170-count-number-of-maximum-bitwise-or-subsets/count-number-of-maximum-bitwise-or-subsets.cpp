d4e8d553763c3729025f5217c89166340db5dda6
Clement-Codes <clementcodes@gmail.com>
2024-10-18 21:38:57 +0100

    Time: 2587 ms (5.11%) | Memory: 536.5 MB (5.24%) - LeetSync

--- 2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.cpp
+++ 2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.cpp
@@ -23,18 +23,39 @@ public:
     //     return result;
     // }
 
-    int dfs(int index, int curresult, vector<int>& nums, int& max_or){
+    //Brute Force without variable
+    // int dfs(int index, int curresult, vector<int>& nums, int& max_or){
+    //     if (index == nums.size())
+    //     {
+    //         return curresult == max_or ? 1 : 0 ;
+    //     }
+    //     return dfs(index + 1, curresult, nums, max_or) + dfs(index + 1, curresult | nums[index], nums, max_or);
+    // }
+
+    // int countMaxOrSubsets(vector<int>& nums) {
+    //     int max_or = 0;
+    //     for(int i: nums)
+    //         max_or |= i;
+    //     return dfs(0, 0, nums, max_or);
+    // }
+
+    // Memoization
+    int dfs(int index, int curresult, vector<int>& nums, int& max_or, unordered_map<string, int> cache){
+        string key = to_string(index) + "_" + to_string(curresult);
+        if (cache.contains(key)) return cache[key];
         if (index == nums.size())
         {
             return curresult == max_or ? 1 : 0 ;
         }
-        return dfs(index + 1, curresult, nums, max_or) + dfs(index + 1, curresult | nums[index], nums, max_or);
+        cache[key] = dfs(index + 1, curresult, nums, max_or, cache) + dfs(index + 1, curresult | nums[index], nums, max_or, cache);
+        return cache[key];
     }
 
     int countMaxOrSubsets(vector<int>& nums) {
         int max_or = 0;
         for(int i: nums)
             max_or |= i;
-        return dfs(0, 0, nums, max_or);
+        unordered_map<string, int> cache;
+        return dfs(0, 0, nums, max_or, cache);
     }
 };
