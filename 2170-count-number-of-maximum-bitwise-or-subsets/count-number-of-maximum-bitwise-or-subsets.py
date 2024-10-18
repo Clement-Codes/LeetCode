3775095ed390e8bc682b666d5d1c6d93fccf03d5
Clement-Codes <clementcodes@gmail.com>
2024-10-18 21:34:31 +0100

    Time: 15 ms (100.00%) | Memory: 17.5 MB (27.27%) - LeetSync

--- 2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.py
+++ 2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.py
@@ -18,13 +18,25 @@ class Solution:
         # return result 
 
         # Brute Force with no variable
+        # max_or = 0
+        # for i in nums:
+        #     max_or |= i
+        # def dfs(i, curr):
+        #     if i  == len(nums): 
+        #         return 1 if curr == max_or else 0
+        #     return dfs(i + 1, curr) + dfs(i + 1, curr | nums[i])
+        # return dfs(0, 0) 
+
+        #Memoization
         max_or = 0
         for i in nums:
             max_or |= i
-
+        cache = {}
         def dfs(i, curr):
+            if (i, curr) in cache:
+                return cache[(i, curr)]
             if i  == len(nums): 
                 return 1 if curr == max_or else 0
-            return dfs(i + 1, curr) + dfs(i + 1, curr | nums[i])
-        
+            cache[(i, curr)] = dfs(i + 1, curr) + dfs(i + 1, curr | nums[i])
+            return cache[(i, curr)]
         return dfs(0, 0) 
