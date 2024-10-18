6cd536911b06df5fffe8e32741440958e684f424
Clement-Codes <clementcodes@gmail.com>
2024-10-19 00:19:27 +0100

    Time: 15 ms (100.00%) | Memory: 16.8 MB (33.55%) - LeetSync

--- 2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.py
+++ 2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.py
@@ -28,15 +28,32 @@ class Solution:
         # return dfs(0, 0) 
 
         #Memoization
+        # max_or = 0
+        # for i in nums:
+        #     max_or |= i
+        # cache = {}
+        # def dfs(i, curr):
+        #     if (i, curr) in cache:
+        #         return cache[(i, curr)]
+        #     if i  == len(nums): 
+        #         return 1 if curr == max_or else 0
+        #     cache[(i, curr)] = dfs(i + 1, curr) + dfs(i + 1, curr | nums[i])
+        #     return cache[(i, curr)]
+        # return dfs(0, 0) 
+
+        #bottom-up
         max_or = 0
         for i in nums:
             max_or |= i
         cache = {}
-        def dfs(i, curr):
-            if (i, curr) in cache:
-                return cache[(i, curr)]
-            if i  == len(nums): 
-                return 1 if curr == max_or else 0
-            cache[(i, curr)] = dfs(i + 1, curr) + dfs(i + 1, curr | nums[i])
-            return cache[(i, curr)]
-        return dfs(0, 0) 
+        cache[0] = 1
+        i = 0
+        while True:
+            if i == len(nums):
+                break
+            clone = deepcopy(cache)
+            for j in cache.keys():
+                clone[j | nums[i]] = clone.get(j | nums[i], 0) + cache[j]
+            cache = clone
+            i += 1
+        return cache[max_or]
