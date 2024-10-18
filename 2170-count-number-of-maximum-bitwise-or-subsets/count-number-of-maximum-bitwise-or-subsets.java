14196c27bd9b85f6b6fcb85f120b9e350ae096d1
Clement-Codes <clementcodes@gmail.com>
2024-10-19 00:51:03 +0100

    Time: 15 ms (47.73%) | Memory: 44.1 MB (36.36%) - LeetSync

--- 2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.java
+++ 2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.java
@@ -37,20 +37,37 @@ class Solution {
     // }
 
     //Memoization
-    public int dfs(int index, int curresult, int[] nums, int max_or, HashMap<String, Integer> cache){
-        String key = String.valueOf(index) + "_" + String.valueOf(curresult);
-        if(cache.containsKey(key)) return cache.get(key);
-        if(index == nums.length) return curresult == max_or ? 1 : 0;
+    // public int dfs(int index, int curresult, int[] nums, int max_or, HashMap<String, Integer> cache){
+    //     String key = String.valueOf(index) + "_" + String.valueOf(curresult);
+    //     if(cache.containsKey(key)) return cache.get(key);
+    //     if(index == nums.length) return curresult == max_or ? 1 : 0;
         
-        cache.put(key,  dfs(index + 1, curresult, nums, max_or, cache) + dfs(index + 1, curresult | nums[index], nums, max_or, cache));
-        return cache.get(key);
-    }
+    //     cache.put(key,  dfs(index + 1, curresult, nums, max_or, cache) + dfs(index + 1, curresult | nums[index], nums, max_or, cache));
+    //     return cache.get(key);
+    // }
 
+    // public int countMaxOrSubsets(int[] nums) {
+    //     HashMap<String, Integer> cache = new HashMap<String, Integer>();
+    //     int max_or = 0;
+    //     for (int i: nums)
+    //         max_or |= i;
+    //     return dfs(0, 0, nums, max_or, cache);
+    // }
+
+    //Bottom up
     public int countMaxOrSubsets(int[] nums) {
-        HashMap<String, Integer> cache = new HashMap<String, Integer>();
         int max_or = 0;
         for (int i: nums)
             max_or |= i;
-        return dfs(0, 0, nums, max_or, cache);
+        HashMap<Integer, Integer> cache = new HashMap<Integer, Integer>();
+        cache.put(0, 1);
+        for(int num: nums){
+            HashMap<Integer, Integer> temp = new HashMap<Integer, Integer>(cache);
+            for(Map.Entry<Integer, Integer> j: cache.entrySet()){
+                temp.put(num | j.getKey(), temp.getOrDefault(num | j.getKey(), 0) + j.getValue());
+            }
+            cache = temp;
+        }
+        return cache.get(max_or);
     }
 }
