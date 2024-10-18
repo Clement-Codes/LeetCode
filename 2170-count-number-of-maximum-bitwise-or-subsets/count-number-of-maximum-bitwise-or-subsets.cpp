6ebfd3e92b252235d5ef486d8ae42944ff60a323
Clement-Codes <clementcodes@gmail.com>
2024-10-18 20:50:39 +0100

    Time: 9 ms (88.63%) | Memory: 10 MB (82.38%) - LeetSync

--- 2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.cpp
+++ 2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.cpp
@@ -1,24 +1,40 @@
 class Solution {
 public:
-    void dfs(int index, int curresult, vector<int>& nums, int& max_or, int& result){
+    // Brute Force
+    // void dfs(int index, int curresult, vector<int>& nums, int& max_or, int& result){
+    //     if (index == nums.size())
+    //     {
+    //         if(curresult == max_or){
+    //             result += 1;
+    //         }
+    //         return;
+    //     }
+
+    //     dfs(index + 1, curresult, nums, max_or, result);
+    //     dfs(index + 1, curresult | nums[index], nums, max_or, result);
+    // }
+
+    // int countMaxOrSubsets(vector<int>& nums) {
+    //     int max_or = 0;
+    //     int result = 0;
+    //     for(int i: nums)
+    //         max_or |= i;
+    //     dfs(0, 0, nums, max_or, result);
+    //     return result;
+    // }
+
+    int dfs(int index, int curresult, vector<int>& nums, int& max_or){
         if (index == nums.size())
         {
-            if(curresult == max_or){
-                result += 1;
-            }
-            return;
+            return curresult == max_or ? 1 : 0 ;
         }
-
-        dfs(index + 1, curresult, nums, max_or, result);
-        dfs(index + 1, curresult | nums[index], nums, max_or, result);
+        return dfs(index + 1, curresult, nums, max_or) + dfs(index + 1, curresult | nums[index], nums, max_or);
     }
 
     int countMaxOrSubsets(vector<int>& nums) {
         int max_or = 0;
-        int result = 0;
         for(int i: nums)
             max_or |= i;
-        dfs(0, 0, nums, max_or, result);
-        return result;
+        return dfs(0, 0, nums, max_or);
     }
 };
