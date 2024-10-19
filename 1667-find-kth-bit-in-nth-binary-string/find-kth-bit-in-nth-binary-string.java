aab8bca02ce46695bc1069f8e43b431c9b38dcf8
Clement-Codes <clementcodes@gmail.com>
2024-10-19 12:44:35 +0100

    Time: 0 ms (100.00%) | Memory: 40.7 MB (73.93%) - LeetSync

---
+++ 1667-find-kth-bit-in-nth-binary-string/find-kth-bit-in-nth-binary-string.java
@@ -0,0 +1,44 @@
+class Solution {
+    //recursion with variable
+    public char recurssion(int length, int k){
+        if (length == 1) return '0';
+        int half = length / 2;
+        if (k <= half){
+            return recurssion(half, k);
+        }
+        else if (k > half + 1){
+            char res = recurssion(half, 1 + length - k );
+            return res == '1' ? '0' : '1';
+        }
+        else{
+            return '1';
+        }
+    }
+
+    // recurssion without variable
+    // char recurssion(int length, int k, bool invert){
+    //     if (length == 1) return invert == false ? '0' : '1';
+    //     int half = length / 2;
+    //     if (k <= half) return recurssion(half, k, invert);
+    //     else if (k > half + 1) return recurssion(half, 1 + length - k, !invert);
+    //     else return invert == false ? '1' : '0';
+    // }
+
+    public char findKthBit(int n, int k) {
+        int length = (int)Math.pow(2, n) - 1;
+        return recurssion(length, k);
+        // // iterative
+        // bool invert = false;
+        // while (length > 1){
+        //     int half = length / 2;
+        //     if (k <= half) length = half;
+        //     else if (k > half + 1){
+        //         k = 1 + length - k;
+        //         length = half;
+        //         invert = !invert;
+        //     }
+        //     else return invert == false ? '1' : '0';
+        // }
+        // return invert == false ? '0' : '1'; 
+    }
+}
