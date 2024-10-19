d978c739b8f95b3a953ad584911970dc8d71dc0e
Clement-Codes <clementcodes@gmail.com>
2024-10-19 12:43:05 +0100

    Time: 0 ms (100.00%) | Memory: 7.4 MB (64.16%) - LeetSync

--- 1667-find-kth-bit-in-nth-binary-string/find-kth-bit-in-nth-binary-string.cpp
+++ 1667-find-kth-bit-in-nth-binary-string/find-kth-bit-in-nth-binary-string.cpp
@@ -17,16 +17,30 @@ public:
     // }
 
     // recurssion without variable
-    char recurssion(int length, int k, bool invert){
-        if (length == 1) return invert == false ? '0' : '1';
-        int half = length / 2;
-        if (k <= half) return recurssion(half, k, invert);
-        else if (k > half + 1) return recurssion(half, 1 + length - k, !invert);
-        else return invert == false ? '1' : '0';
-    }
+    // char recurssion(int length, int k, bool invert){
+    //     if (length == 1) return invert == false ? '0' : '1';
+    //     int half = length / 2;
+    //     if (k <= half) return recurssion(half, k, invert);
+    //     else if (k > half + 1) return recurssion(half, 1 + length - k, !invert);
+    //     else return invert == false ? '1' : '0';
+    // }
+
 
     char findKthBit(int n, int k) {
         int length = pow(2, n) - 1;
-        return recurssion(length, k, false);
+        // return recurssion(length, k, false);
+        // iterative
+        bool invert = false;
+        while (length > 1){
+            int half = length / 2;
+            if (k <= half) length = half;
+            else if (k > half + 1){
+                k = 1 + length - k;
+                length = half;
+                invert = !invert;
+            }
+            else return invert == false ? '1' : '0';
+        }
+        return invert == false ? '0' : '1';
     }
 };
