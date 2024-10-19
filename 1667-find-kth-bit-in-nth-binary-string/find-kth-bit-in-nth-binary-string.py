084f18ff54536fb95a9b790a302832bf8d9f8aa9
Clement-Codes <clementcodes@gmail.com>
2024-10-19 11:35:53 +0100

    Time: 0 ms (100.00%) | Memory: 16.6 MB (72.78%) - LeetSync

--- 1667-find-kth-bit-in-nth-binary-string/find-kth-bit-in-nth-binary-string.py
+++ 1667-find-kth-bit-in-nth-binary-string/find-kth-bit-in-nth-binary-string.py
@@ -1,22 +1,36 @@
 class Solution:
     
     def findKthBit(self, n: int, k: int) -> str:
-        cache = {}
-        def inverse(strs):
-            result = ""
-            for s in strs:
-                if s == '0':
-                    result += '1'
-                else:
-                    result += '0'
-            return result
-
-        def dfs(i):
-            if i in cache: return cache[i]
-            if i == 1:
+        
+        def helper(length, k):
+            if length == 1:
                 return '0'
+            half = length // 2
+            if k <= half:
+                return helper(half, k)
+            elif k > half + 1:
+                res = helper(half, 1 + length - k)
+                return '0' if res == '1' else '1'
+            else:
+                return '1'
 
-            cache[i] = dfs(i-1) + "1" + inverse(dfs(i-1))[::-1]
-            return cache[i]
+        return helper(2**n - 1, k)
+        # cache = {}
+        # def inverse(strs):
+        #     result = ""
+        #     for s in strs:
+        #         if s == '0':
+        #             result += '1'
+        #         else:
+        #             result += '0'
+        #     return result
 
-        return dfs(n)[k - 1]
+        # def dfs(i):
+        #     if i in cache: return cache[i]
+        #     if i == 1:
+        #         return '0'
+
+        #     cache[i] = dfs(i-1) + "1" + inverse(dfs(i-1))[::-1]
+        #     return cache[i]
+
+        # return dfs(n)[k - 1]
