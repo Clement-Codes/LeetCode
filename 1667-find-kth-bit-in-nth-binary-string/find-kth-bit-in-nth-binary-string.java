586db1bba3da7f76b455887ca5df6b9041b27528
Clement-Codes <clementcodes@gmail.com>
2024-10-19 12:45:16 +0100

    Time: 0 ms (100.00%) | Memory: 41 MB (60.31%) - LeetSync

--- 1667-find-kth-bit-in-nth-binary-string/find-kth-bit-in-nth-binary-string.java
+++ 1667-find-kth-bit-in-nth-binary-string/find-kth-bit-in-nth-binary-string.java
@@ -1,32 +1,32 @@
 class Solution {
     //recursion with variable
-    public char recurssion(int length, int k){
-        if (length == 1) return '0';
-        int half = length / 2;
-        if (k <= half){
-            return recurssion(half, k);
-        }
-        else if (k > half + 1){
-            char res = recurssion(half, 1 + length - k );
-            return res == '1' ? '0' : '1';
-        }
-        else{
-            return '1';
-        }
-    }
-
-    // recurssion without variable
-    // char recurssion(int length, int k, bool invert){
-    //     if (length == 1) return invert == false ? '0' : '1';
+    // public char recurssion(int length, int k){
+    //     if (length == 1) return '0';
     //     int half = length / 2;
-    //     if (k <= half) return recurssion(half, k, invert);
-    //     else if (k > half + 1) return recurssion(half, 1 + length - k, !invert);
-    //     else return invert == false ? '1' : '0';
+    //     if (k <= half){
+    //         return recurssion(half, k);
+    //     }
+    //     else if (k > half + 1){
+    //         char res = recurssion(half, 1 + length - k );
+    //         return res == '1' ? '0' : '1';
+    //     }
+    //     else{
+    //         return '1';
+    //     }
     // }
+
+    //recurssion without variable
+    public char recurssion(int length, int k, boolean invert){
+        if (length == 1) return invert == false ? '0' : '1';
+        int half = length / 2;
+        if (k <= half) return recurssion(half, k, invert);
+        else if (k > half + 1) return recurssion(half, 1 + length - k, !invert);
+        else return invert == false ? '1' : '0';
+    }
 
     public char findKthBit(int n, int k) {
         int length = (int)Math.pow(2, n) - 1;
-        return recurssion(length, k);
+        return recurssion(length, k, false);
         // // iterative
         // bool invert = false;
         // while (length > 1){
