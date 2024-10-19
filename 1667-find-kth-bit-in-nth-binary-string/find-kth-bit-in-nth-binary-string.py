b0ea01b20d5d28370ca7ed347d289f19386d1e32
Clement-Codes <clementcodes@gmail.com>
2024-10-19 11:52:00 +0100

    Time: 0 ms (100.00%) | Memory: 16.5 MB (88.95%) - LeetSync

--- 1667-find-kth-bit-in-nth-binary-string/find-kth-bit-in-nth-binary-string.py
+++ 1667-find-kth-bit-in-nth-binary-string/find-kth-bit-in-nth-binary-string.py
@@ -4,18 +4,31 @@ class Solution:
         invert = False
         length = 2**n - 1
 
-        def helper(length, k, invert):
-            if length == 1:
-                return '0' if not invert  else '1'
+        while length > 1:
             half = length // 2
             if k <= half:
-                return helper(half, k, invert)
+                length = half
             elif k > half + 1:
-                return helper(half, 1 + length - k, not invert)
+                k = 1 + length - k
+                length = half
+                invert = not invert
             else:
                 return '1' if not invert  else '0'
+        return '0' if not invert  else '1'
 
-        return helper(length, k, invert)
+        # helper function
+        # def helper(length, k, invert):
+        #     if length == 1:
+        #         return '0' if not invert  else '1'
+        #     half = length // 2
+        #     if k <= half:
+        #         return helper(half, k, invert)
+        #     elif k > half + 1:
+        #         return helper(half, 1 + length - k, not invert)
+        #     else:
+        #         return '1' if not invert  else '0'
+
+        # return helper(length, k, invert)
 
         # cache = {}
         # def inverse(strs):
