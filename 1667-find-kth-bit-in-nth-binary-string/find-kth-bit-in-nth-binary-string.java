95f84c0776e947e9e48441951993c0a2e8822e6e
Clement-Codes <clementcodes@gmail.com>
2024-10-19 12:45:45 +0100

    Time: 0 ms (100.00%) | Memory: 41 MB (64.79%) - LeetSync

--- 1667-find-kth-bit-in-nth-binary-string/find-kth-bit-in-nth-binary-string.java
+++ 1667-find-kth-bit-in-nth-binary-string/find-kth-bit-in-nth-binary-string.java
@@ -16,29 +16,29 @@ class Solution {
     // }
 
     //recurssion without variable
-    public char recurssion(int length, int k, boolean invert){
-        if (length == 1) return invert == false ? '0' : '1';
-        int half = length / 2;
-        if (k <= half) return recurssion(half, k, invert);
-        else if (k > half + 1) return recurssion(half, 1 + length - k, !invert);
-        else return invert == false ? '1' : '0';
-    }
+    // public char recurssion(int length, int k, boolean invert){
+    //     if (length == 1) return invert == false ? '0' : '1';
+    //     int half = length / 2;
+    //     if (k <= half) return recurssion(half, k, invert);
+    //     else if (k > half + 1) return recurssion(half, 1 + length - k, !invert);
+    //     else return invert == false ? '1' : '0';
+    // }
 
     public char findKthBit(int n, int k) {
         int length = (int)Math.pow(2, n) - 1;
-        return recurssion(length, k, false);
-        // // iterative
-        // bool invert = false;
-        // while (length > 1){
-        //     int half = length / 2;
-        //     if (k <= half) length = half;
-        //     else if (k > half + 1){
-        //         k = 1 + length - k;
-        //         length = half;
-        //         invert = !invert;
-        //     }
-        //     else return invert == false ? '1' : '0';
-        // }
-        // return invert == false ? '0' : '1'; 
+        //return recurssion(length, k, false);
+        // iterative
+        boolean invert = false;
+        while (length > 1){
+            int half = length / 2;
+            if (k <= half) length = half;
+            else if (k > half + 1){
+                k = 1 + length - k;
+                length = half;
+                invert = !invert;
+            }
+            else return invert == false ? '1' : '0';
+        }
+        return invert == false ? '0' : '1'; 
     }
 }
