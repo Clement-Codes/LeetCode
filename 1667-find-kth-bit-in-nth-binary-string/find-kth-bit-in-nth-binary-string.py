e86b127ffb5eba58e1cebda4007b8ca6d3f09893
Clement-Codes <clementcodes@gmail.com>
2024-10-19 11:39:57 +0100

    Time: 0 ms (100.00%) | Memory: 16.6 MB (72.78%) - LeetSync

--- 1667-find-kth-bit-in-nth-binary-string/find-kth-bit-in-nth-binary-string.py
+++ 1667-find-kth-bit-in-nth-binary-string/find-kth-bit-in-nth-binary-string.py
@@ -1,20 +1,22 @@
 class Solution:
     
     def findKthBit(self, n: int, k: int) -> str:
-        
-        def helper(length, k):
+        invert = False
+        length = 2**n - 1
+
+        def helper(length, k, invert):
             if length == 1:
-                return '0'
+                return '0' if not invert  else '1'
             half = length // 2
             if k <= half:
-                return helper(half, k)
+                return helper(half, k, invert)
             elif k > half + 1:
-                res = helper(half, 1 + length - k)
-                return '0' if res == '1' else '1'
+                return helper(half, 1 + length - k, not invert)
             else:
-                return '1'
+                return '1' if not invert  else '0'
 
-        return helper(2**n - 1, k)
+        return helper(length, k, invert)
+
         # cache = {}
         # def inverse(strs):
         #     result = ""
