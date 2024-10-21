30c5be8aea814db863137c27cb9e934237001fa1
Clement-Codes <clementcodes@gmail.com>
2024-10-21 11:24:42 +0100

    Time: 187 ms (59.42%) | Memory: 16.6 MB (55.61%) - LeetSync

--- 1715-split-a-string-into-the-max-number-of-unique-substrings/split-a-string-into-the-max-number-of-unique-substrings.py
+++ 1715-split-a-string-into-the-max-number-of-unique-substrings/split-a-string-into-the-max-number-of-unique-substrings.py
@@ -3,17 +3,17 @@ class Solution:
         result = 0
         
         def dfs(i, curr):
-            nonlocal result
             if i == len(s):
-                
-                result = max(result, len(curr))
-                return 
+                return 0
+
+            result = 0
             for j in range(i + 1, len(s) + 1):
                 if s[i:j] in curr:
                     continue
-                temp = set(curr)
-                temp.add(s[i:j])
-                dfs(j, temp)
-
-        dfs(0, set())
-        return result
+                
+                curr.add(s[i:j])
+                result = max(result, 1 + dfs(j, curr))
+                curr.remove(s[i:j])
+            return result
+            
+        return dfs(0, set())
