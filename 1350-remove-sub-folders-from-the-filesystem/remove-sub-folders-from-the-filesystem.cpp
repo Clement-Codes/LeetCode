6a59243c30499c51c3b8b15203b449d9dec842f1
Clement-Codes <clementcodes@gmail.com>
2024-10-25 13:18:54 +0100

    Time: 555 ms (5.22%) | Memory: 217.5 MB (7.69%) - LeetSync

--- 1350-remove-sub-folders-from-the-filesystem/remove-sub-folders-from-the-filesystem.cpp
+++ 1350-remove-sub-folders-from-the-filesystem/remove-sub-folders-from-the-filesystem.cpp
@@ -1,18 +1,66 @@
+#include <string.h>
+#include <stdio.h>
+using namespace std;
+class Trie {
+public:
+    unordered_map<string, Trie*> children;
+    bool endOfWord = false;
+
+public:
+    void add(string s){
+        cout << s;
+        Trie* curr = this;
+        stringstream st(s);
+        string t;
+        while (getline(st, t, '/')){
+            if (!curr->children.contains(t)) curr->children[t] = new Trie();
+            curr = curr->children[t];
+        }
+        curr->endOfWord = true;
+    }
+
+    bool prefix(string s){
+        Trie* curr = this;
+        vector<string> temp;
+        stringstream ss(s);
+        string t;
+        while (getline(ss, t, '/')) temp.push_back(t);
+        temp.pop_back();
+        for(string sss: temp){
+            curr = curr->children[sss];
+            if (curr->endOfWord) return true;
+        }
+        return false;
+    }
+};
+
 class Solution {
 public:
     vector<string> removeSubfolders(vector<string>& folder) {
+        Trie* curr = new Trie();
         vector<string> result;
-        unordered_set<string> cache;
-        for (string i: folder) cache.insert(i);
-        for (string i: folder){
-            result.push_back(i);
-            for (int j = 0; j < i.length(); j++){
-                if (i[j] == '/' && cache.contains(i.substr(0, j))){
-                    result.pop_back();
-                    break;
-                }
-            }
-        }
+        for (string temp: folder)
+            curr->add(temp);
+
+        for (string temp: folder)
+            if(! curr->prefix(temp)) result.push_back(temp);
+
         return result;
     }
+
+    // vector<string> removeSubfolders(vector<string>& folder) {
+    //     vector<string> result;
+    //     unordered_set<string> cache;
+    //     for (string i: folder) cache.insert(i);
+    //     for (string i: folder){
+    //         result.push_back(i);
+    //         for (int j = 0; j < i.length(); j++){
+    //             if (i[j] == '/' && cache.contains(i.substr(0, j))){
+    //                 result.pop_back();
+    //                 break;
+    //             }
+    //         }
+    //     }
+    //     return result;
+    // }
 };
