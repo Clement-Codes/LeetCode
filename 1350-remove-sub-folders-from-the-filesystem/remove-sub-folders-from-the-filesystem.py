21d93d4a3a4909fc1aee39ba6ca43ace9b821a1f
Clement-Codes <clementcodes@gmail.com>
2024-10-25 12:00:31 +0100

    Time: 150 ms (69.14%) | Memory: 41.4 MB (23.28%) - LeetSync

--- 1350-remove-sub-folders-from-the-filesystem/remove-sub-folders-from-the-filesystem.py
+++ 1350-remove-sub-folders-from-the-filesystem/remove-sub-folders-from-the-filesystem.py
@@ -1,12 +1,44 @@
+class Trie:
+    def __init__(self):
+        self.childrens = {}
+        self.end = False
+
 class Solution:
     def removeSubfolders(self, folder: List[str]) -> List[str]:
-        cache = set(folder)
+        cache = Trie()
         result = []
-        for i in folder:
-            result.append(i)
-            for j in range(len(i)):
-                if i[j] == "/" and i[:j] in cache:
-                    result.pop()
-                    break
+
+        for f in folder:
+            temp = cache
+            for d in f[1:].split('/'):
+                if d not in temp.childrens:
+                    temp.childrens[d] = Trie() 
+                temp = temp.childrens[d] 
+            temp.end = True
+        
+        
+
+        stack = [(cache, "")]
+        while stack:
+            temp1, temp2 = stack.pop(0)
+            if temp1.end:
+                result.append(temp2)
+                continue
+            for i, j in temp1.childrens.items():
+                stack.append((j, temp2+'/'+i))
+
+
+            
+
         return result
+
+        # cache = set(folder)
+        # result = []
+        # for i in folder:
+        #     result.append(i)
+        #     for j in range(len(i)):
+        #         if i[j] == "/" and i[:j] in cache:
+        #             result.pop()
+        #             break
+        # return result
 
