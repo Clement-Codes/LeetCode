bfa69506ebbb54215de73c0c92c11dd0d69a7fe0
Clement-Codes <clementcodes@gmail.com>
2024-10-27 12:04:21 +0000

    Time: 189 ms (9.76%) | Memory: 57 MB (6.11%) - LeetSync

--- 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.java
+++ 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.java
@@ -1,22 +1,40 @@
 class Solution {
-    int ROW = 0, COL = 0;
-
-    int dfs(int r, int c, int[][] matrix, HashMap<String, Integer> memo){
-        String key = String.valueOf(r) + "_" + String.valueOf(c);
-        if (memo.containsKey(key)) return memo.get(key);
-        if (r == ROW || c == COL || matrix[r][c] == 0) return 0;
-        memo.put(key, 1 + Math.min(dfs(r + 1, c, matrix, memo), Math.min(dfs(r, c + 1, matrix, memo), dfs(r + 1, c + 1, matrix, memo))));
-        return memo.get(key);
-    }
-
     public int countSquares(int[][] matrix) {
-        int result = 0;
-        ROW = matrix.length;
-        COL = matrix[0].length;
+        int ROW = matrix.length, COL = matrix[0].length, result = 0;
         HashMap<String, Integer> memo = new HashMap<String, Integer>();
         for (int r = 0; r < ROW; r++)
-            for (int c = 0; c < COL; c++)
-                result += dfs(r, c, matrix, memo);
+            for (int c = 0; c < COL; c++){
+                if (r > 0 && c > 0 && matrix[r][c] != 0){
+                    memo.put(String.valueOf(r) + "_" + String.valueOf(c), 1 + Math.min(memo.get(String.valueOf(r - 1) + "_" + String.valueOf(c)),
+                    Math.min(memo.get(String.valueOf(r) + "_" + String.valueOf(c - 1)), memo.get(String.valueOf(r - 1) + "_" + String.valueOf(c - 1)))));
+                    result +=  memo.get(String.valueOf(r) + "_" + String.valueOf(c));
+                }
+                else{
+                    memo.put(String.valueOf(r) + "_" + String.valueOf(c), matrix[r][c]);
+                    result += matrix[r][c];
+                }             
+            }
         return result;
     }
+    // dfs
+    // int ROW = 0, COL = 0;
+
+    // int dfs(int r, int c, int[][] matrix, HashMap<String, Integer> memo){
+    //     String key = String.valueOf(r) + "_" + String.valueOf(c);
+    //     if (memo.containsKey(key)) return memo.get(key);
+    //     if (r == ROW || c == COL || matrix[r][c] == 0) return 0;
+    //     memo.put(key, 1 + Math.min(dfs(r + 1, c, matrix, memo), Math.min(dfs(r, c + 1, matrix, memo), dfs(r + 1, c + 1, matrix, memo))));
+    //     return memo.get(key);
+    // }
+
+    // public int countSquares(int[][] matrix) {
+    //     int result = 0;
+    //     ROW = matrix.length;
+    //     COL = matrix[0].length;
+    //     HashMap<String, Integer> memo = new HashMap<String, Integer>();
+    //     for (int r = 0; r < ROW; r++)
+    //         for (int c = 0; c < COL; c++)
+    //             result += dfs(r, c, matrix, memo);
+    //     return result;
+    // }
 }
