8c5d91f7f425fdbda40cfb92849eeb677efb242e
Clement-Codes <clementcodes@gmail.com>
2024-10-28 14:11:35 +0000

    Time: 138 ms (26.67%) | Memory: 108.9 MB (75.24%) - LeetSync

--- 2586-longest-square-streak-in-an-array/longest-square-streak-in-an-array.cpp
+++ 2586-longest-square-streak-in-an-array/longest-square-streak-in-an-array.cpp
@@ -1,13 +1,30 @@
 class Solution {
 public:
     int longestSquareStreak(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
+        unordered_set<double> nums2;
         int result = 0;
-        unordered_map<float, int> memo;
-        for (int i: nums){
-            memo[i] = memo[sqrt(i)] + 1;
-            result = max(result, memo[i]);
+        for (int i: nums) nums2.insert(i);
+        for (int i: nums2){
+            if (nums2.contains(sqrt(i))) continue;
+            int j = 1;
+            int temp_i = i;
+            while ( pow(i, 2) < 100000 and nums2.contains(i * i)){
+                j += 1;
+                i *= i;
+            } 
+            result = max(result, j);
         }
-        return result > 1 ? result : -1; 
+        return result > 1 ? result : -1;
     }
+    // Sorting
+    // int longestSquareStreak(vector<int>& nums) {
+    //     sort(nums.begin(), nums.end());
+    //     int result = 0;
+    //     unordered_map<float, int> memo;
+    //     for (int i: nums){
+    //         memo[i] = memo[sqrt(i)] + 1;
+    //         result = max(result, memo[i]);
+    //     }
+    //     return result > 1 ? result : -1; 
+    // }
 };
