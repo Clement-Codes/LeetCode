43f9627b548875cc5600ab50b3de265ba6708f53
Clement-Codes <clementcodes@gmail.com>
2024-11-03 19:31:55 +0000

    Time: 0 ms (100.00%) | Memory: 7.8 MB (37.13%) - LeetSync

---
+++ 812-rotate-string/rotate-string.cpp
@@ -0,0 +1,7 @@
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        string t = s + s;
+        return s.length() == goal.length() && t.find(goal) != string::npos;
+    }
+};
