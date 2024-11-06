f35ae543c31d746bb64265afa7d4569dcc744e2d
Clement-Codes <clementcodes@gmail.com>
2024-11-06 10:43:35 +0000

    Time: 8 ms (21.43%) | Memory: 44 MB (92.45%) - LeetSync

---
+++ 3291-find-if-array-can-be-sorted/find-if-array-can-be-sorted.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int getBit(int n){
+        int result = 0;
+        while (n > 0){
+            result += n & 1;
+            n = n >> 1;
+        }
+        return result;
+    }
+
+    public boolean canSortArray(int[] nums) {
+        for (int i = 0; i < nums.length; i++)
+        {
+            boolean swap = false;
+            for (int j = 0; j < nums.length - i - 1; j++)
+            {
+                if (nums[j] < nums[j + 1]) continue;
+                if (getBit(nums[j]) == getBit(nums[j + 1]))
+                {
+                    int temp = nums[j];
+                    nums[j] = nums[j + 1];
+                    nums[j + 1] = temp;
+                    swap = true;
+                }
+                else{
+                    return false;
+                } 
+            } 
+            if (!swap) break;
+        }
+        return true;
+    }
+}
