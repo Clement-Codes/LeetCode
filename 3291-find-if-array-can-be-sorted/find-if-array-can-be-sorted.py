e4cf9c0b43e829428ddd308b99dd97a763bb9b85
Clement-Codes <clementcodes@gmail.com>
2024-11-06 10:33:53 +0000

    Time: 333 ms (5.45%) | Memory: 16.5 MB (78.85%) - LeetSync

--- 3291-find-if-array-can-be-sorted/find-if-array-can-be-sorted.py
+++ 3291-find-if-array-can-be-sorted/find-if-array-can-be-sorted.py
@@ -1,47 +1,48 @@
 class Solution:
     def canSortArray(self, nums: List[int]) -> bool:
+        #0(n2) using bubble sort
         def getBit(n):
             temp = 0
             while n:
                 temp += n & 1
                 n  = n >> 1 
             return temp
-        min_curr, max_curr = nums[0], nums[0]
-        prev_curr = float(-inf)
-
-        for n in nums:
-            if getBit(n) == getBit(min_curr):
-                min_curr = min(min_curr, n)
-                max_curr = max(max_curr, n)
-            else:
-                if min_curr < prev_curr:
+        
+        si, sj = 0, 0 
+        for i in range(len(nums)):
+            swap = False
+            for j in range(len(nums) - i - 1):
+                if nums[j] < nums[j + 1]:
+                    continue
+                if getBit(nums[j]) == getBit(nums[j + 1]):
+                    swap = True
+                    nums[j], nums[j + 1] = nums[j + 1], nums[j]
+                else:
                     return False
-                prev_curr = max_curr
-                min_curr, max_curr = n, n
-        return False if min_curr < prev_curr else True
-        # edge = True
-        # for i in range(1, len(nums)):
-        #     if nums[i - 1] > nums[i]:
-        #         edge = False
-        # if edge:
-        #     return True
+            if not swap:
+                break
+        return True 
 
-        # cache = defaultdict(list)
-        # for i in range(len(nums)):
-        #     n = nums[i]
+
+
+        #0(n)
+        # def getBit(n):
         #     temp = 0
         #     while n:
-        #         if n & 1:
-        #             temp += 1
-        #         n = n >> 1
-        #     cache[temp].append(nums[i])
-        # print(cache)
-        # temp = []
-        # for i, j in cache.items():
-        #     temp.extend(sorted(j))
+        #         temp += n & 1
+        #         n  = n >> 1 
+        #     return temp
+        # min_curr, max_curr = nums[0], nums[0]
+        # prev_curr = float(-inf)
 
-        # print(temp)
-        # for i in range(1, len(temp)):
-        #     if temp[i - 1] > temp[i]:
-        #         return False
-        # return True
+        # for n in nums:
+        #     if getBit(n) == getBit(min_curr):
+        #         min_curr = min(min_curr, n)
+        #         max_curr = max(max_curr, n)
+        #     else:
+        #         if min_curr < prev_curr:
+        #             return False
+        #         prev_curr = max_curr
+        #         min_curr, max_curr = n, n
+        # return False if min_curr < prev_curr else True
+
