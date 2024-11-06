eed563b60a8271e6785af452ded0cf8c45c69c2d
Clement-Codes <clementcodes@gmail.com>
2024-11-06 10:52:05 +0000

    Time: 0 ms (100.00%) | Memory: 31 MB (99.47%) - LeetSync

--- 3291-find-if-array-can-be-sorted/find-if-array-can-be-sorted.cpp
+++ 3291-find-if-array-can-be-sorted/find-if-array-can-be-sorted.cpp
@@ -8,27 +8,45 @@ public:
         }
         return result;
     }
+
     bool canSortArray(vector<int>& nums) {
-        
-        for (int i = 0; i < nums.size(); i++)
-        {
-            bool swap = false;
-            for (int j = 0; j < nums.size() - i - 1; j++)
-            {
-                if (nums[j] < nums[j + 1]) continue;
-                if (getBit(nums[j]) == getBit(nums[j + 1]))
-                {
-                    int temp = nums[j];
-                    nums[j] = nums[j + 1];
-                    nums[j + 1] = temp;
-                    swap = true;
-                }
-                else{
-                    return false;
-                } 
-            } 
-            if (!swap) break;
+        int min_curr = nums[0], max_curr = nums[0], prev_curr = INT_MIN;
+        for (int n: nums){
+            if (getBit(n) == getBit(min_curr)){
+                min_curr = min(min_curr, n);
+                max_curr = max(max_curr, n);
+            }
+            else{
+                if (min_curr < prev_curr) return false;
+                prev_curr = max_curr;
+                min_curr = n;
+                max_curr = n;
+            }
         }
-        return true;
+        return min_curr < prev_curr ? false : true;
     }
+    // O(N2) Bubble Sort
+    // bool canSortArray(vector<int>& nums) {
+        
+    //     for (int i = 0; i < nums.size(); i++)
+    //     {
+    //         bool swap = false;
+    //         for (int j = 0; j < nums.size() - i - 1; j++)
+    //         {
+    //             if (nums[j] < nums[j + 1]) continue;
+    //             if (getBit(nums[j]) == getBit(nums[j + 1]))
+    //             {
+    //                 int temp = nums[j];
+    //                 nums[j] = nums[j + 1];
+    //                 nums[j + 1] = temp;
+    //                 swap = true;
+    //             }
+    //             else{
+    //                 return false;
+    //             } 
+    //         } 
+    //         if (!swap) break;
+    //     }
+    //     return true;
+    // }
 };
