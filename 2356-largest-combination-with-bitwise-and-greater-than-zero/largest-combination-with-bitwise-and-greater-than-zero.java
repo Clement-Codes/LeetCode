ce07e0c936cdabb8645175371e07c06aa5de7598
Clement-Codes <clementcodes@gmail.com>
2024-11-07 02:12:53 +0000

    Time: 11 ms (98.41%) | Memory: 57 MB (19.52%) - LeetSync

--- 2356-largest-combination-with-bitwise-and-greater-than-zero/largest-combination-with-bitwise-and-greater-than-zero.java
+++ 2356-largest-combination-with-bitwise-and-greater-than-zero/largest-combination-with-bitwise-and-greater-than-zero.java
@@ -1,28 +1,28 @@
 class Solution {
     public int largestCombination(int[] candidates) {
-        int result = 0;
-        for (int i = 0; i < 32; i++){
-            int count = 0;
-            for (int n: candidates){
-                count += (1 << i & n) > 0 ? 1: 0;
-            }
-            result = Math.max(result, count);
-        }
-        return result;
-
-        // int[] count = new int[32];
-        // Arrays.fill(count, 0);
-        // for (int n: candidates){
-        //     int i = 0;
-        //     while (n > 0){
-        //         count[i] += n & 1;
-        //         n = n >> 1;
-        //         i += 1;
-        //     }
-        // }
         // int result = 0;
-        // for (int n: count)
-        //     if (result < n) result = n;
+        // for (int i = 0; i < 32; i++){
+        //     int count = 0;
+        //     for (int n: candidates){
+        //         count += (1 << i & n) > 0 ? 1: 0;
+        //     }
+        //     result = Math.max(result, count);
+        // }
         // return result;
+
+        int[] count = new int[32];
+        Arrays.fill(count, 0);
+        for (int n: candidates){
+            int i = 0;
+            while (n > 0){
+                count[i] += n & 1;
+                n = n >> 1;
+                i += 1;
+            }
+        }
+        int result = 0;
+        for (int n: count)
+            if (result < n) result = n;
+        return result;
     }
 }
