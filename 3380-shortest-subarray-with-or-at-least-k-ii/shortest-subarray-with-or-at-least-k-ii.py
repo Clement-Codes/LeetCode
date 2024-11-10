02b42167513bed573e4f032fe4100de222fa540a
Clement-Codes <clementcodes@gmail.com>
2024-11-10 13:09:57 +0000

    Time: 2179 ms (18.60%) | Memory: 35.8 MB (69.92%) - LeetSync

---
+++ 3380-shortest-subarray-with-or-at-least-k-ii/shortest-subarray-with-or-at-least-k-ii.py
@@ -0,0 +1,30 @@
+class Solution:
+    def minimumSubarrayLength(self, nums: List[int], k: int) -> int:
+        result = float("inf")
+        bits = [0] * 32
+
+        def bitOperation(bits, num, diff):
+            for i in range(32):
+                if num & (1 << i):
+                    bits[i] += diff
+
+        def getInteger(bits):
+            temp = 0
+            for i in range(32):
+                if bits[i] > 0:
+                    temp += (1 << i)
+            return temp
+
+        l = 0
+        
+
+        for r in range(len(nums)):
+            bitOperation(bits, nums[r], 1)
+            curr = getInteger(bits)
+            print(curr, r)
+            while curr >= k and l <= r:
+                result = min(result, r - l + 1)
+                bitOperation(bits, nums[l], -1)
+                curr = getInteger(bits)
+                l += 1
+        return -1 if result == float("inf") else result
