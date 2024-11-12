d137d889f17bc687c55d85e3170cc0d209a552da
Clement-Codes <clementcodes@gmail.com>
2024-11-12 00:00:25 +0000

    Time: 9 ms (30.05%) | Memory: 27.4 MB (78.76%) - LeetSync

--- 2716-prime-subtraction-operation/prime-subtraction-operation.cpp
+++ 2716-prime-subtraction-operation/prime-subtraction-operation.cpp
@@ -5,15 +5,34 @@ public:
             if (n % i == 0)
                 return false;
         return true;
-    } 
+    }
+
+    int getMax(vector<int>& nums){
+        int res = INT_MIN;
+        for(int i: nums)
+            if (res < i)
+                res = i;
+        return res;
+    }
 
     bool primeSubOperation(vector<int>& nums) {
         int prev = 0;
+
+        vector<bool> primes = {false, false};
+        int maxi = getMax(nums);
+        for (int i = 2; i < maxi; i++)
+            if (isPrime(i)){
+                primes.push_back(true);
+            }
+            else{
+                primes.push_back(false);
+            }
+
         for (int n: nums){
             int upperbound = n - prev;
             int largest_prime = 0;
             for (int i = upperbound - 1; i >=2; i--){
-                if (isPrime(i)){
+                if (primes[i]){
                     largest_prime = i;
                     break;
                 }
