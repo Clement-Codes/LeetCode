aa147e1ddaa4d714d2fe788651e84db9d9d8abae
Clement-Codes <clementcodes@gmail.com>
2024-11-12 00:28:45 +0000

    Time: 16 ms (7.63%) | Memory: 44.7 MB (50.99%) - LeetSync

--- 2716-prime-subtraction-operation/prime-subtraction-operation.java
+++ 2716-prime-subtraction-operation/prime-subtraction-operation.java
@@ -17,29 +17,31 @@ class Solution {
     public boolean primeSubOperation(int[] nums) {
         int prev = 0;
 
-        List<Boolean> primes = new ArrayList<Boolean>();
+        List<Integer> primes = new ArrayList<Integer>();
         int maxi = getMax(nums);
-        primes.add(false);
-        primes.add(false);
+        primes.add(0);
+        primes.add(0);
         for (int i = 2; i < maxi; i++)
             if (isPrime(i)){
-                primes.add(true);
+                primes.add(i);
             }
             else{
-                primes.add(false);
+                primes.add(primes.get(primes.size() - 1));
             }
 
         for (int n: nums){
             int upperbound = n - prev;
-            int largest_prime = 0;
-            for (int i = upperbound - 1; i >=2; i--){
-                // if (isPrime(i)){
-                if (primes.get(i)){
+            int largest_prime = upperbound - 1 >=0 ? primes.get(upperbound - 1) : primes.get(primes.size()  + (upperbound - 1));
 
-                    largest_prime = i;
-                    break;
-                }
-            }
+            //int largest_prime = 0;
+            // for (int i = upperbound - 1; i >=2; i--){
+            //     // if (isPrime(i)){
+            //     if (primes.get(i)){
+
+            //         largest_prime = i;
+            //         break;
+            //     }
+            // }
             if (n  - largest_prime <= prev)
                 return false;
             prev = n - largest_prime;
