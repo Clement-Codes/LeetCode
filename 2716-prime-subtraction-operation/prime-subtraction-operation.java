46c343e4bdd9bffa0fde679ef5217610b2997bef
Clement-Codes <clementcodes@gmail.com>
2024-11-12 00:04:29 +0000

    Time: 14 ms (10.17%) | Memory: 44.4 MB (70.86%) - LeetSync

--- 2716-prime-subtraction-operation/prime-subtraction-operation.java
+++ 2716-prime-subtraction-operation/prime-subtraction-operation.java
@@ -6,8 +6,29 @@ class Solution {
         return true;
     } 
 
+    public int getMax(int[] nums){
+        int res = Integer.MIN_VALUE;
+        for(int i: nums)
+            if (res < i)
+                res = i;
+        return res;
+    }
+    
     public boolean primeSubOperation(int[] nums) {
         int prev = 0;
+
+        List<Boolean> primes = new ArrayList<Boolean>();
+        int maxi = getMax(nums);
+        primes.add(false);
+        primes.add(false);
+        for (int i = 2; i < maxi; i++)
+            if (isPrime(i)){
+                primes.add(true);
+            }
+            else{
+                primes.add(false);
+            }
+
         for (int n: nums){
             int upperbound = n - prev;
             int largest_prime = 0;
