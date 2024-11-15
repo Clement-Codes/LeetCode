33f54ddcfda4da92f70b5c18643d3d6157a7fae1
Clement-Codes <clementcodes@gmail.com>
2024-11-15 13:09:28 +0000

    Time: 12 ms (68.47%) | Memory: 30.4 MB (78.50%) - LeetSync

--- 1679-shortest-subarray-to-be-removed-to-make-array-sorted/shortest-subarray-to-be-removed-to-make-array-sorted.py
+++ 1679-shortest-subarray-to-be-removed-to-make-array-sorted/shortest-subarray-to-be-removed-to-make-array-sorted.py
@@ -6,18 +6,18 @@ class Solution:
             r -= 1
         res = r
         
-        l = 0
-        while l < n  and arr[l] <= arr[l + 1]:
-            l += 1
+        # l = 0
+        # while l < n  and arr[l] <= arr[l + 1]:
+        #     l += 1
         
-        res = min(res, n - l)
+        # res = min(res, n - l)
 
         l = 0
-        r = n
+        #r = n
 
         while l < r:
-            while r <= n and l + 1 < r and arr[r - 1] <= arr[r] and arr[l] <= arr[r]:
-                r -= 1
+            # while r <= n and l + 1 < r and arr[r - 1] <= arr[r] and arr[l] <= arr[r]:
+            #     r -= 1
             
             while r <= n  and arr[l] > arr[r]:
                 r += 1
