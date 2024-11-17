3bf39ac3b31d8bf659354e91d519175d128a16a6
Clement-Codes <clementcodes@gmail.com>
2024-11-17 12:45:07 +0000

    Time: 53 ms (36.42%) | Memory: 112.7 MB (48.92%) - LeetSync

--- 892-shortest-subarray-with-sum-at-least-k/shortest-subarray-with-sum-at-least-k.cpp
+++ 892-shortest-subarray-with-sum-at-least-k/shortest-subarray-with-sum-at-least-k.cpp
@@ -1,25 +1,46 @@
 class Solution {
 public:
-    static bool compare(pair<long, int> a, pair<long, int>b){
-        return a.first > b.first;
-    }
     int shortestSubarray(vector<int>& nums, int k) {
-        //Using Heap
-        long s = 0;
-        int result = INT_MAX;
-        priority_queue<pair<long, int>, vector<pair<long, int>>, function<bool(pair<long, int>, pair<long, int>)>> pq(compare);
-        for (int r = 0; r < nums.size(); r++){
+       deque<pair<long, int>> d;
+       long s = 0;
+       int result = INT_MAX;
+       for (int r = 0; r < nums.size(); r++){
             s += nums[r];
             if (s >= k) result = min(result, r + 1);
-            
-            while (pq.size() > 0  && s - pq.top().first >= k){
-                pair<long, int> temp = pq.top();
-                pq.pop();
+
+            while (d.size() > 0 && s - d.front().first >= k){
+                pair<long, int> temp = d.front();
+                d.pop_front();
                 result = min(result, r - temp.second);
             }
 
-            pq.push(pair<long, int>{s, r});
-        }
-        return result == INT_MAX ? -1 : result;
+            while (d.size() > 0 && d.back().first >= s)
+                d.pop_back();
+
+            d.push_back(pair<long, int>(s, r));
+       }
+       return result == INT_MAX ? -1 : result;
     }
+    // static bool compare(pair<long, int> a, pair<long, int>b){
+    //     return a.first > b.first;
+    // }
+    // int shortestSubarray(vector<int>& nums, int k) {
+    //     //Using Heap
+    //     long s = 0;
+    //     int result = INT_MAX;
+    //     priority_queue<pair<long, int>, vector<pair<long, int>>, function<bool(pair<long, int>, pair<long, int>)>> pq(compare);
+    //     for (int r = 0; r < nums.size(); r++){
+    //         s += nums[r];
+    //         if (s >= k) result = min(result, r + 1);
+            
+    //         while (pq.size() > 0  && s - pq.top().first >= k){
+    //             pair<long, int> temp = pq.top();
+    //             pq.pop();
+    //             result = min(result, r - temp.second);
+    //         }
+
+    //         pq.push(pair<long, int>{s, r});
+    //     }
+    //     return result == INT_MAX ? -1 : result;
+    // }
 };
