8866d9f865d4de7ce6ba5ec466cd5e724897740b
Clement-Codes <clementcodes@gmail.com>
2024-11-17 12:08:18 +0000

    Time: 230 ms (33.41%) | Memory: 31.7 MB (51.43%) - LeetSync

--- 892-shortest-subarray-with-sum-at-least-k/shortest-subarray-with-sum-at-least-k.py
+++ 892-shortest-subarray-with-sum-at-least-k/shortest-subarray-with-sum-at-least-k.py
@@ -1,6 +1,8 @@
 class Solution:
     def shortestSubarray(self, nums: List[int], k: int) -> int:
-        heap = []
+        #Using Deque
+        d = deque()
+        l = 0
         result = float("inf")
         s = 0
         for r in range(len(nums)):
@@ -9,9 +11,27 @@ class Solution:
             if s >= k:
                 result = min(result, r + 1)
             
-            while heap and s - heap[0][0] >= k:
-                temp = heapq.heappop(heap)
+            while d and s - d[0][0] >= k:
+                temp = d.popleft()
                 result = min(result, r - temp[1])
-            heapq.heappush(heap, (s, r))
-
+            
+            while d and d[-1][0] >= s:
+                d.pop()
+            d.append((s, r))
         return -1 if result == float("inf") else result
+        # Using Heap
+        # heap = []
+        # result = float("inf")
+        # s = 0
+        # for r in range(len(nums)):
+        #     s += nums[r]
+
+        #     if s >= k:
+        #         result = min(result, r + 1)
+            
+        #     while heap and s - heap[0][0] >= k:
+        #         temp = heapq.heappop(heap)
+        #         result = min(result, r - temp[1])
+        #     heapq.heappush(heap, (s, r))
+
+        # return -1 if result == float("inf") else result
