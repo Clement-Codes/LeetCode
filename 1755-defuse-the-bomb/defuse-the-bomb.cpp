5be85d9dbbaf4215588eb3bad61ff7be9a9110dd
Clement-Codes <clementcodes@gmail.com>
2024-11-18 21:30:41 +0000

    Time: 0 ms (100.00%) | Memory: 10.2 MB (83.96%) - LeetSync

--- 1755-defuse-the-bomb/defuse-the-bomb.cpp
+++ 1755-defuse-the-bomb/defuse-the-bomb.cpp
@@ -2,14 +2,38 @@ class Solution {
 public:
     vector<int> decrypt(vector<int>& code, int k) {
         int n = code.size();
+        int l = 0;
+        int sum = 0;
         vector<int> result(n, 0);
-        for (int i = 0; i < n; i++)
-            if (k > 0)
-                for (int j = i + 1; j < i + k + 1; j++)
-                    result[i] += code[j % n];
-            else if (k < 0)
-                for (int j = i - 1; j > i - abs(k) -1 ; j--)
-                    result[i] += code[(j + n)% n];                    
+        for (int r = 0; r < n + abs(k); r++){
+            sum += code[r % n];
+            if (r - l + 1 > abs(k)){
+                sum -= code[l];
+                l = (l + 1) % n;
+            }
+
+            if (r - l + 1 == abs(k)){
+                if (k > 0){
+                    result[(l - 1 + n) % n] = sum;
+                }
+                else if (k < 0){
+                    result[(r + 1 + n) % n] = sum;
+                }
+            }
+
+        }
         return result;
+
+        // Using loops
+        // int n = code.size();
+        // vector<int> result(n, 0);
+        // for (int i = 0; i < n; i++)
+        //     if (k > 0)
+        //         for (int j = i + 1; j < i + k + 1; j++)
+        //             result[i] += code[j % n];
+        //     else if (k < 0)
+        //         for (int j = i - 1; j > i - abs(k) -1 ; j--)
+        //             result[i] += code[(j + n)% n];                    
+        // return result;
     }
 };
