f59b6c68250ff5bce94ce2be7edde55d8d8e9a79
Clement-Codes <clementcodes@gmail.com>
2024-11-19 02:25:47 +0000

    Time: 117 ms (57.49%) | Memory: 36.7 MB (10.47%) - LeetSync

--- 2552-maximum-sum-of-distinct-subarrays-with-length-k/maximum-sum-of-distinct-subarrays-with-length-k.py
+++ 2552-maximum-sum-of-distinct-subarrays-with-length-k/maximum-sum-of-distinct-subarrays-with-length-k.py
@@ -2,24 +2,44 @@ class Solution:
     def maximumSubarraySum(self, nums: List[int], k: int) -> int:
         n = len(nums)
         result = 0
-        visited = defaultdict(int)
         l = 0
         s = 0
+        prev_index = {}
         for r in range(n):
-            visited[nums[r]] += 1
             s += nums[r]
+            i = prev_index.get(nums[r], -1)
 
-            if r - l + 1 > k:
-                if visited[nums[l]] == 1:
-                    del visited[nums[l]]
-                else:
-                    visited[nums[l]] -= 1
+            while l <= i or r - l + 1 > k:
                 s -= nums[l]
                 l += 1
             
-            if r - l + 1 == k and len(visited) == k:
+            if r - l + 1 == k:
                 result = max(result, s)
+            
+            prev_index[nums[r]] = r
         return result        
+
+        #Using map
+        # n = len(nums)
+        # result = 0
+        # visited = defaultdict(int)
+        # l = 0
+        # s = 0
+        # for r in range(n):
+        #     visited[nums[r]] += 1
+        #     s += nums[r]
+
+        #     if r - l + 1 > k:
+        #         if visited[nums[l]] == 1:
+        #             del visited[nums[l]]
+        #         else:
+        #             visited[nums[l]] -= 1
+        #         s -= nums[l]
+        #         l += 1
+            
+        #     if r - l + 1 == k and len(visited) == k:
+        #         result = max(result, s)
+        # return result        
 
         # n = len(nums)
         # l = 0
