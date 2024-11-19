bc51a61c11b998431356f3955ee9178dc4a4c4b0
Clement-Codes <clementcodes@gmail.com>
2024-11-19 11:11:21 +0000

    Time: 100 ms (84.98%) | Memory: 98.2 MB (52.46%) - LeetSync

--- 2552-maximum-sum-of-distinct-subarrays-with-length-k/maximum-sum-of-distinct-subarrays-with-length-k.cpp
+++ 2552-maximum-sum-of-distinct-subarrays-with-length-k/maximum-sum-of-distinct-subarrays-with-length-k.cpp
@@ -1,31 +1,54 @@
 class Solution {
 public:
     long long maximumSubarraySum(vector<int>& nums, int k) {
+        //loops with skip
         long long result = 0;
         int l = 0;
         int n = nums.size();
         long long sum = 0;
-        unordered_map<int, int> counter;
+        unordered_map<int, int> prevPos;
         for (int r = 0; r < n; r++){
             sum += nums[r];
-            if (counter.find(nums[r]) != counter.end())
-                counter[nums[r]] += 1;
-            else
-                counter[nums[r]] = 1;
 
-            if (r - l + 1 > k){
-                counter[nums[l]] -= 1;
-                if (counter[nums[l]] == 0)
-                    counter.erase(nums[l]);
+            while (l <= (prevPos.contains(nums[r]) ? prevPos[nums[r]] : -1) || r - l + 1 > k){
                 sum -= nums[l];
                 l += 1;
             }
 
 
-            if (r - l + 1 == k && counter.size() == k){
+            if (r - l + 1 == k){
                 result = max(result, sum);
             }
+
+            prevPos[nums[r]] = r;
         }
-        return result;
+        return result;        
+        // loops
+        // long long result = 0;
+        // int l = 0;
+        // int n = nums.size();
+        // long long sum = 0;
+        // unordered_map<int, int> counter;
+        // for (int r = 0; r < n; r++){
+        //     sum += nums[r];
+        //     if (counter.find(nums[r]) != counter.end())
+        //         counter[nums[r]] += 1;
+        //     else
+        //         counter[nums[r]] = 1;
+
+        //     if (r - l + 1 > k){
+        //         counter[nums[l]] -= 1;
+        //         if (counter[nums[l]] == 0)
+        //             counter.erase(nums[l]);
+        //         sum -= nums[l];
+        //         l += 1;
+        //     }
+
+
+        //     if (r - l + 1 == k && counter.size() == k){
+        //         result = max(result, sum);
+        //     }
+        // }
+        // return result;
     }
 };
