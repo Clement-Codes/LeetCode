afcb6747081d8d86baaee13ccc9c12d8dfec93f6
Clement-Codes <clementcodes@gmail.com>
2024-11-23 14:21:29 +0000

    Time: 182 ms (86.25%) | Memory: 60.9 MB (10.98%) - LeetSync

---
+++ 1972-rotating-the-box/rotating-the-box.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<vector<char>> rotateTheBox(vector<vector<char>>& box) {
+        int ROW = box.size();
+        int COL = box[0].size();
+        
+        for (int r = 0; r < ROW; r++){
+            int i = COL - 1;
+            for (int c = COL - 1; c >= 0; c--){
+                if (box[r][c] == '#'){
+                    int temp = box[r][i];
+                    box[r][i] = box[r][c];
+                    box[r][c] = temp;
+                    i -= 1;
+                }
+                else if(box[r][c] == '*'){
+                    i = c - 1;
+                }
+            }
+        }
+
+        vector<vector<char>> result;
+        for (int i = 0; i < COL; i++){
+            vector<char> temp;
+            for (int j = ROW - 1; j >=0; j--){
+                temp.push_back(box[j][i]);
+            }
+            result.push_back(temp);
+        }
+        return result;
+    }
+};
