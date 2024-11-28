95e8db9f26c43ab163ac7605b6f48044838d58d1
Clement-Codes <clementcodes@gmail.com>
2024-11-28 19:09:45 +0000

    Time: 813 ms (5.00%) | Memory: 77.9 MB (38.06%) - LeetSync

--- 2375-minimum-obstacle-removal-to-reach-corner/minimum-obstacle-removal-to-reach-corner.java
+++ 2375-minimum-obstacle-removal-to-reach-corner/minimum-obstacle-removal-to-reach-corner.java
@@ -1,9 +1,10 @@
 class Solution {
+    //deque
     public int minimumObstacles(int[][] grid) {
         int ROW = grid.length;
         int COL = grid[0].length;
 
-        PriorityQueue<List<Integer>> pq = new PriorityQueue<>((x, y) -> Integer.compare(x.get(0),y.get(0)));
+        Deque<List<Integer>> pq = new LinkedList<>();
         HashSet<String> visited = new HashSet<String>();
         pq.add(new ArrayList<Integer>(Arrays.asList(0,0,0)));
         visited.add(String.valueOf(0) + '_' + String.valueOf(0));
@@ -20,11 +21,49 @@ class Solution {
                 String key = String.valueOf(x) + '_' + String.valueOf(y);
                 if (0 > x || 0 > y || x == ROW || y == COL || visited.contains(key))
                     continue;
-                List<Integer> t = new ArrayList<Integer>(Arrays.asList(grid[x][y] + temp.get(0), x, y));
-                pq.add(t);
+
+                if (grid[x][y] > 0){
+                List<Integer> t = new ArrayList<Integer>(Arrays.asList(1 + temp.get(0), x, y));
+                pq.addLast(t);
+                }
+                else{
+                List<Integer> t = new ArrayList<Integer>(Arrays.asList(temp.get(0), x, y));
+                pq.addFirst(t);
+                }
+                
                 visited.add(key);
             }
         }
         return 0; 
     }
 }
+    // priority queue
+    // public int minimumObstacles(int[][] grid) {
+    //     int ROW = grid.length;
+    //     int COL = grid[0].length;
+
+    //     PriorityQueue<List<Integer>> pq = new PriorityQueue<>((x, y) -> Integer.compare(x.get(0),y.get(0)));
+    //     HashSet<String> visited = new HashSet<String>();
+    //     pq.add(new ArrayList<Integer>(Arrays.asList(0,0,0)));
+    //     visited.add(String.valueOf(0) + '_' + String.valueOf(0));
+    //     int directions[][] = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+
+    //     while (pq.size() > 0){
+    //         List<Integer> temp = pq.poll();
+    //         if (temp.get(1) == ROW - 1 && temp.get(2) == COL - 1){
+    //             return temp.get(0);
+    //         }
+    //         for (int[] direction: directions){
+    //             int x = temp.get(1) + direction[0];
+    //             int y = temp.get(2) + direction[1];
+    //             String key = String.valueOf(x) + '_' + String.valueOf(y);
+    //             if (0 > x || 0 > y || x == ROW || y == COL || visited.contains(key))
+    //                 continue;
+    //             List<Integer> t = new ArrayList<Integer>(Arrays.asList(grid[x][y] + temp.get(0), x, y));
+    //             pq.add(t);
+    //             visited.add(key);
+    //         }
+    //     }
+    //     return 0; 
+    // }
+//}
