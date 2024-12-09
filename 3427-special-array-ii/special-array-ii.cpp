020492758fc110e9726cb6eef9de266b1b4a86da
Clement-Codes <clementcodes@gmail.com>
2024-12-09 04:18:58 +0000

    Time: 4 ms (87.50%) | Memory: 126.7 MB (60.13%) - LeetSync

---
+++ 3427-special-array-ii/special-array-ii.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<bool> isArraySpecial(vector<int>& nums, vector<vector<int>>& queries) {
+        vector<bool> result(queries.size(), false);
+        vector<int> prefix(nums.size(), 0);
+        for (int i = 0; i < nums.size() - 1; i++)
+        
+            if (nums[i] % 2 == nums[i + 1] % 2)
+            {
+                prefix[i + 1] = prefix[i] + 1;
+            }
+            else{
+                prefix[i + 1] = prefix[i];
+            }
+        for (int i = 0; i < queries.size(); i++){
+            result[i] = prefix[queries[i][1]] - prefix[queries[i][0]] == 0;
+        }
+        return result;
+    }
+};
