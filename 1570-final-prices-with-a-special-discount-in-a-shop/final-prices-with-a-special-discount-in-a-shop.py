b90fe93988886c5902a26e121922d74fddff82e7
Clement-Codes <clementcodes@gmail.com>
2024-12-18 01:03:12 +0000

    Time: 3 ms (55.13%) | Memory: 17.7 MB (10.22%) - LeetSync

--- 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.py
+++ 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.py
@@ -1,15 +1,25 @@
 class Solution:
     def finalPrices(self, prices: List[int]) -> List[int]:
         n = len(prices)
-        l = 0
-        while l < n:
-            r = l + 1
-            while r < n and prices[l] < prices[r]:
-                r += 1
-            if r == n:
-                prices[l] = prices[l]
-                l += 1
-            else:
-                prices[l] = (prices[l] - prices[r])
-                l, r = l + 1, r + 1
+        deq = deque()
+        for l in range(n):
+            while len(deq) > 0 and prices[l] <= prices[deq[-1]]:
+                r = deq.pop()
+                prices[r] -= prices[l]
+            deq.append(l)
         return prices
+
+        # kind of two pointers basically for loops i guess
+        # n = len(prices)
+        # l = 0
+        # while l < n:
+        #     r = l + 1
+        #     while r < n and prices[l] < prices[r]:
+        #         r += 1
+        #     if r == n:
+        #         prices[l] = prices[l]
+        #         l += 1
+        #     else:
+        #         prices[l] = (prices[l] - prices[r])
+        #         l, r = l + 1, r + 1
+        # return prices
