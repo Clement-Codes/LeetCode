a9c4e8b2ec388de2fe33a8e8b6c3d88e27eaf33b
Clement-Codes <clementcodes@gmail.com>
2024-12-20 14:52:36 +0000

    Time: 0 ms (100.00%) | Memory: 51.8 MB (22.92%) - LeetSync

--- 3379-score-of-a-string/score-of-a-string.js
+++ 3379-score-of-a-string/score-of-a-string.js
@@ -5,7 +5,7 @@
 var scoreOfString = function(s) {
     let result = 0;
     for (let i = 0; i < s.length - 1; i++){
-        result += Math.abs(s[i].charCodeAt(0) - s[i + 1].charCodeAt(0));
+        result += Math.abs(s.charCodeAt(i) - s.charCodeAt(i + 1));
     }
     return result;
 };
