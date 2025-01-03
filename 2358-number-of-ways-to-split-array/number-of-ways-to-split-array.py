34e3151538208dbd8f1b0160ef08fff95366867b
Clement-Codes <clementcodes@gmail.com>
2025-01-03 17:13:05 +0000

    Time: 54 ms (68.60%) | Memory: 32.3 MB (58.91%) - LeetSync

--- 2358-number-of-ways-to-split-array/number-of-ways-to-split-array.py
+++ 2358-number-of-ways-to-split-array/number-of-ways-to-split-array.py
@@ -1,12 +1,9 @@
 class Solution:
     def waysToSplitArray(self, nums: List[int]) -> int:
-        cache = [0] * (len(nums) + 1)
-        for i, num in enumerate(nums):
-            cache[i + 1] = num + cache[i]
         s = sum(nums)
-
         result = 0
-        for l in range(0, len(nums) - 1):
-            if (cache[l + 1] >= s - cache[l + 1]):
-                result += 1
+        l = 0
+        for i in range(0, len(nums) - 1):
+            l += nums[i] 
+            result += 1 if l >= s - l else 0
         return result
