b29a395fad07ddb74d633cd73e934b08c2e1995f
Clement-Codes <clementcodes@gmail.com>
2025-01-15 22:55:01 +0000

    Time: 3 ms (17.46%) | Memory: 17.9 MB (23.02%) - LeetSync

---
+++ 2509-minimize-xor/minimize-xor.py
@@ -0,0 +1,30 @@
+class Solution:
+    def minimizeXor(self, num1: int, num2: int) -> int:
+        def countBit(n: int) -> int:
+            result = 0
+            while n > 0:
+                if n & 1:
+                    result += 1
+                n = n >> 1
+            return result
+        
+        count1, count2 = countBit(num1), countBit(num2)
+        result = num1
+        print("yeap")
+        i = 0
+        while count1 > count2:
+            
+            if result & (1 << i):
+                result = result ^ (1 << i)
+                count1 -= 1
+            i += 1
+        
+
+        while count1 < count2:
+            if result & (1 << i) == 0:
+                result = result ^ (1 << i)
+                count1 += 1
+            i += 1
+
+        return result
+    
